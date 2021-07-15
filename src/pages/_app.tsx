c8a9fdbb01dd8113781fa5eb1a35d1122e600f79
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2021-07-15 15:14:33 -0400

    Add progress bar on page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import { Layout } from '@components';
 import { rootService } from '@machines';
 import { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
+import 'nprogress/nprogress.css';
 // import 'public/katex/katex.css';
 import React, { FC } from 'react';
 import 'tailwindcss/tailwind.css';
 
+const TopProgressBar = dynamic(() => import('@components/TopProgressBar').then((mod) => mod.TopProgressBar), {
+  ssr: false,
+});
+
 const NectarApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} service={rootService} />
-    </Layout>
+    <>
+      <TopProgressBar />
+      <Layout>
+        <Component {...pageProps} service={rootService} />
+      </Layout>
+    </>
   );
 };
 
