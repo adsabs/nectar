d79ffe9ebdf6367f70308bb65535910fddf33e1f
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2021-08-10 04:46:56 -0400

    add result-list tests

---
+++ src/components/__tests__/ResultList.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Default as ResultList } from '../__stories__/ResultList.stories';
+
+describe('ResultList', () => {
+  it('renders without crashing', () => {
+    render(<ResultList />);
+  });
+});
