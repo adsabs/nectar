6f83ce2e94f2b2a531eca3a5cf3626d61bf20621
Jennifer Chen <shinyi.chen@gmail.com>
2021-09-16 12:51:51 -0400

    Fixed layout for responsiveness

--- src/components/ResultList/Item/Item.tsx
+++ src/components/ResultList/Item/Item.tsx
@@ -34,7 +34,7 @@ export const Item = (props: IItemProps): React.ReactElement => {
 
   const checkBgClass = clsx(
     state.matches('selected') ? 'bg-blue-600' : 'bg-gray-100',
-    'hidden items-center justify-center mr-3 px-2 rounded-bl-md rounded-tl-md md:flex',
+    'flex items-center justify-center mr-3 px-2 rounded-bl-md rounded-tl-md',
   );
 
   const indexClass = clsx(
@@ -57,36 +57,38 @@ export const Item = (props: IItemProps): React.ReactElement => {
           />
         </div>
       )}
-      <div className="flex flex-1 flex-col px-2 py-3">
-        <div className="flex justify-between">
+      <div className="flex flex-col w-full">
+        <div className="flex flex-row justify-between">
           <Link href={`/abs/${bibcode}`}>
-            <a className="py-1 hover:underline text-xs">{bibcode}</a>
+            <a className="pt-4 hover:underline text-sm">{bibcode}</a>
           </Link>
+          <div className="flex items-start p-2">
+            <button title="Full text sources" tabIndex={0}>
+              <DocumentTextIcon className="default-icon default-link-color" />
+            </button>
+            <button title="Citations and references" tabIndex={0}>
+              <ViewListIcon className="default-icon default-link-color" />
+            </button>
+            <button title="Data" tabIndex={0}>
+              <DatabaseIcon className="default-icon default-link-color" />
+            </button>
+          </div>
         </div>
-        <Link href={`/abs/${bibcode}`}>
-          <a className="text-blue-700 hover:underline">
-            <h3 className="text-lg" id={`result-${id}`} dangerouslySetInnerHTML={{ __html: title }}></h3>
-          </a>
-        </Link>
-        {author.length > 0 && <div className="text-s">{author.slice(0, 3).join('; ')}</div>}
-        <div className="flex py-1">
-          {pubdate && <span className="text-xs">{pubdate}</span>}
-          {citation && <span className="text-xs">cite: {citation}</span>}
+        <div className="flex flex-col px-2 py-3">
+          <Link href={`/abs/${bibcode}`}>
+            <a className="text-blue-700 hover:underline">
+              <h3 className="text-lg" id={`result-${id}`} dangerouslySetInnerHTML={{ __html: title }}></h3>
+            </a>
+          </Link>
+          {author.length > 0 && <div className="text-s">{author.slice(0, 3).join('; ')}</div>}
+          <div className="flex py-1">
+            {pubdate && <span className="text-xs">{pubdate}</span>}
+            {citation && <span className="text-xs">cite: {citation}</span>}
+          </div>
+          <div className="flex">
+            <AbstractPreview id={id} />
+          </div>
         </div>
-        <div className="flex">
-          <AbstractPreview id={id} />
-        </div>
-      </div>
-      <div className="flex items-start p-2">
-        <button title="Full text sources" tabIndex={0}>
-          <DocumentTextIcon className="default-icon default-link-color" />
-        </button>
-        <button title="Citations and references" tabIndex={0}>
-          <ViewListIcon className="default-icon default-link-color" />
-        </button>
-        <button title="Data" tabIndex={0}>
-          <DatabaseIcon className="default-icon default-link-color" />
-        </button>
       </div>
     </article>
   );
