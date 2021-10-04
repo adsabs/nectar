7eded3ad47c8e880816c952b7e8179b43f9eba96
Jennifer Chen <shinyi.chen@gmail.com>
2021-10-04 17:00:38 -0400

    Error handling

--- src/pages/abs/[id]/metrics.tsx
+++ src/pages/abs/[id]/metrics.tsx
@@ -2,93 +2,93 @@ import AdsApi, { IADSApiMetricsParams, IADSApiMetricsResponse, IDocsEntity, IUse
 import { AbsLayout } from '@components/Layout/AbsLayout';
 import { GetServerSideProps, NextPage } from 'next';
 import React from 'react';
-import DefaultErrorPage from 'next/error';
-import { normalizeURLParams } from '@utils';
-import { abstractPageNavDefaultQueryFields } from '@components/AbstractSideNav/model';
+import { getDocument, normalizeURLParams } from '@utils';
 import { getCitationTableData, getReadsTableData, plotCitationsHist, plotReadsHist } from '@graphUtils';
 import { CitationsTable } from '@components/Metrics/Citations/Table';
 import { ReadsTable } from '@components/Metrics/Reads/Table';
 interface IMetricsPageProps {
   metrics: IADSApiMetricsResponse;
-  doc: IDocsEntity;
+  originalDoc: IDocsEntity;
   error?: Error;
 }
 
 const MetricsPage: NextPage<IMetricsPageProps> = (props: IMetricsPageProps) => {
-  const { doc, error, metrics } = props;
+  const { originalDoc, error, metrics } = props;
 
-  if (error) {
-    return <DefaultErrorPage statusCode={404} title={error.message} />;
-  }
+  const hasCitations = metrics && metrics['citation stats']['total number of citations'] > 0;
 
-  const hasCitations = metrics['citation stats']['total number of citations'] > 0;
+  const hasReads = metrics && metrics['basic stats']['total number of reads'] > 0;
 
-  const hasReads = metrics['basic stats']['total number of reads'] > 0;
-
-  const hist = metrics.histograms;
+  const hist = metrics ? metrics.histograms : null;
 
   // graph data
-  const citations_graph = {
-    graphData: plotCitationsHist(false, hist.citations),
-    normalizedGraphData: plotCitationsHist(true, hist.citations),
-  };
+  const citations_graph = hasCitations
+    ? {
+        graphData: plotCitationsHist(false, hist.citations),
+        normalizedGraphData: plotCitationsHist(true, hist.citations),
+      }
+    : null;
 
-  const reads_graph = {
-    graphData: plotReadsHist(false, hist.reads),
-    normalizedGraphData: plotReadsHist(true, hist.reads),
-  };
+  const reads_graph = hasReads
+    ? {
+        graphData: plotReadsHist(false, hist.reads),
+        normalizedGraphData: plotReadsHist(true, hist.reads),
+      }
+    : null;
 
   // table data
-  const citations_table = getCitationTableData({
-    refereed: metrics['citation stats refereed'],
-    total: metrics['citation stats'],
-  });
+  const citations_table = hasCitations
+    ? getCitationTableData({
+        refereed: metrics['citation stats refereed'],
+        total: metrics['citation stats'],
+      })
+    : null;
 
-  const reads_table = getReadsTableData({
-    refereed: metrics['basic stats refereed'],
-    total: metrics['basic stats'],
-  });
+  const reads_table = hasReads
+    ? getReadsTableData({
+        refereed: metrics['basic stats refereed'],
+        total: metrics['basic stats'],
+      })
+    : null;
 
   const headingClass = 'bg-gray-100 text-3xl h-16 p-2 font-light flex items-center my-5';
 
   return (
-    <AbsLayout doc={doc}>
+    <AbsLayout doc={originalDoc}>
       <article aria-labelledby="title" className="mx-0 my-10 px-4 w-full bg-white md:mx-2">
         <div className="pb-1">
           <h2 className="prose-xl pb-5 text-gray-900 text-2xl font-medium leading-8" id="title">
-            <span>Metrics for </span> <div className="text-2xl">{doc.title}</div>
+            <span>Metrics for </span> <div className="text-2xl">{originalDoc.title}</div>
           </h2>
         </div>
-        {hasCitations ? (
-          <section>
-            <div className={headingClass}>
-              <h3>Citations</h3>
-            </div>
-            <CitationsTable data={citations_table} isAbstract={true} />
-          </section>
-        ) : null}
-        {hasReads ? (
-          <section>
-            <div className={headingClass}>
-              <h3>Reads</h3>
-            </div>
-            <ReadsTable data={reads_table} isAbstract={true} />
-          </section>
-        ) : null}
+        {error ? (
+          <div className="flex items-center justify-center w-full h-full text-xl">{error}</div>
+        ) : (
+          <>
+            {hasCitations ? (
+              <section>
+                <div className={headingClass}>
+                  <h3>Citations</h3>
+                </div>
+                <CitationsTable data={citations_table} isAbstract={true} />
+              </section>
+            ) : null}
+            {hasReads ? (
+              <section>
+                <div className={headingClass}>
+                  <h3>Reads</h3>
+                </div>
+                <ReadsTable data={reads_table} isAbstract={true} />
+              </section>
+            ) : null}
+          </>
+        )}
       </article>
     </AbsLayout>
   );
 };
 
 export default MetricsPage;
-
-const getOriginalDoc = async (api: AdsApi, id: string) => {
-  const result = await api.search.query({
-    q: `identifier:${id}`,
-    fl: [...abstractPageNavDefaultQueryFields, 'title'],
-  });
-  return result.isOk() ? result.value.docs[0] : null;
-};
 
 export const getServerSideProps: GetServerSideProps<IMetricsPageProps> = async (ctx) => {
   const query = normalizeURLParams(ctx.query);
@@ -101,16 +101,16 @@ export const getServerSideProps: GetServerSideProps<IMetricsPageProps> = async (
   };
   const adsapi = new AdsApi({ token: userData.access_token });
   const result = await adsapi.metrics.query(params);
-  const originalDoc = await getOriginalDoc(adsapi, query.id);
+  const originalDoc = await getDocument(adsapi, query.id);
 
-  return !originalDoc
-    ? { props: { error: 'Document not found' } }
+  return originalDoc.notFound || originalDoc.error
+    ? { notFound: true }
     : result.isErr()
-    ? { props: { metrics: {}, originalDoc, error: result.error } }
+    ? { props: { metrics: [], originalDoc: originalDoc.doc, error: 'Unable to get results' } }
     : {
         props: {
           metrics: result.value,
-          doc: originalDoc,
+          originalDoc: originalDoc.doc,
         },
       };
 };
