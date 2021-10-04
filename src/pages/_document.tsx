cf6703bf0cb647b6d5bf74e60b090f39a32e2bc3
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2021-10-04 03:19:09 -0400

    add Jira issue collector

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): React.ReactElement {
+    return (
+      <Html>
+        <Head>
+          {/* Jira issue collector */}
+          <script
+            type="text/javascript"
+            src="https://adsabs.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/-fzx0du/b/3/9edb822e6c4db3635f27806476c088cb/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=a22da864"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
