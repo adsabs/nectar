c7b315815049915ddd1d19eb9d1e71383daed00f
Jennifer Chen <shinyi.chen@gmail.com>
2021-10-22 13:03:21 -0400

    Use dynamic import

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link';
 import React, { FC } from 'react';
-import { ThemeDropdown } from './ThemeDropdown';
 import styles from './NavBar.module.css';
 import clsx from 'clsx';
-import { NavMenus } from './NavMenus';
-import { isBrowser } from '@utils';
+import dynamic from 'next/dynamic';
 
 export const NavBar: FC = () => {
   const navbarClasses = clsx(styles['navbar-bg-color'], 'relative flex items-center');
+
+  const ThemeDropdown = dynamic(() => import('./ThemeDropdown').then((mod) => mod.ThemeDropdown), { ssr: false });
+
+  const NavMenus = dynamic(() => import('./NavMenus').then((mod) => mod.NavMenus), { ssr: false });
 
   return (
     <nav className={navbarClasses}>
@@ -19,11 +21,11 @@ export const NavBar: FC = () => {
           </a>
         </Link>
       </div>
-      {!isBrowser() ? null : <ThemeDropdown />}
+      <ThemeDropdown />
       <a href="#main-content" className="flex items-center text-white focus:not-sr-only sr-only">
         Skip to content
       </a>
-      {!isBrowser() ? null : <NavMenus />}
+      <NavMenus />
     </nav>
   );
 };
