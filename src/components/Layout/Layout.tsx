1827cd4d3e3746132c38015c04b992c0f8011b87
Jennifer Chen <shinyi.chen@gmail.com>
2021-11-12 09:53:59 -0500

    Document language and title

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,12 +3,16 @@ import { useRouter } from 'next/router';
 import { FC } from 'react';
 import { Footer } from '../Footer';
 import { NavBar } from '../NavBar';
+import Head from 'next/head';
 
 export const Layout: FC = ({ children }) => {
   const router = useRouter();
   const isLandingPages = /^(\/|\/classic-form|\/paper-form)$/.exec(router.asPath);
   return (
     <section className="default-text-color flex flex-col min-h-screen font-sans bg-white">
+      <Head>
+        <title>NASA Science Explorer</title>
+      </Head>
       <NavBar />
       <main>
         {isLandingPages && <LandingTabs />}
