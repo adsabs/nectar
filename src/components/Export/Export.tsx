e07715975634d9d01f767032014d2a5b6945e81c
Jennifer Chen <shinyi.chen@gmail.com>
2021-12-29 12:38:12 -0500

    UI refactor - export

--- src/components/Export/Export.tsx
+++ src/components/Export/Export.tsx
@@ -1,8 +1,4 @@
 import { TextInput } from '@components';
-import { Button } from '@components/Button';
-import { Panel } from '@components/Panel';
-import { Transition } from '@headlessui/react';
-import { DownloadIcon, DuplicateIcon, RefreshIcon } from '@heroicons/react/solid';
 import PT from 'prop-types';
 import { ChangeEvent, HTMLAttributes, ReactElement, useEffect, useRef, useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
@@ -11,6 +7,12 @@ import { FormatSelector } from './FormatSelector';
 import { useExportMachine } from './hook';
 import { LimitRange } from './LimitRange';
 import { ExportState } from './types';
+import { Flex, HStack } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/button';
+import { Collapse, Fade } from '@chakra-ui/transition';
+import { CloseButton } from '@chakra-ui/close-button';
+import { CopyIcon, DownloadIcon } from '@chakra-ui/icons';
+import { Textarea } from '@chakra-ui/react';
 
 export interface IExportProps extends HTMLAttributes<HTMLDivElement> {
   initialFormat?: ExportState['format'];
@@ -62,59 +64,51 @@ export const Export = ({
   const noText = !state.text || state.text.length === 0;
 
   return (
-    <Panel ariaName="export" showCloseButton={!singleMode}>
-      <div className="grid gap-7 grid-cols-12">
-        <div className="col-span-12 sm:col-span-4">
-          <form onSubmit={onSubmit} className="space-y-3">
-            <FormatSelector format={state.format} onFormatChange={onFormatChange} />
+    <Flex
+      direction="column"
+      aria-label="export"
+      boxShadow={!singleMode ? 'md' : 'none'}
+      borderRadius="md"
+      p={!singleMode ? '5' : '0'}
+      gap={2}
+    >
+      {!singleMode && <CloseButton as={Flex} justifyContent="end" width="full" />}
+      <form onSubmit={onSubmit}>
+        <Flex direction="column" gap={3}>
+          <FormatSelector format={state.format} onFormatChange={onFormatChange} />
 
-            <CustomFormatInput
-              show={state.format === 'custom'}
-              customFormat={state.customFormat}
-              onChange={onCustomFormatChange}
-            />
-            <Transition show={!singleMode}>
-              <LimitRange limit={state.limit} max={state.totalRecords} onLimitChange={onLimitChange} />
-            </Transition>
-
-            <div className="flex items-center">
-              <Transition show={!singleMode || state.format === 'custom'}>
-                <Button data-testid="btn-apply" type="submit" disabled={state.loading}>
-                  Apply
-                </Button>
-              </Transition>
-              <Transition show={state.loading} className="ml-1">
-                <RefreshIcon className="w-6 h-6 text-blue-600 animate-spin" />
-              </Transition>
-            </div>
-          </form>
-        </div>
-        <div className="col-span-12 sm:col-span-8">
-          <div className="mb-3">
-            <Button data-testid="btn-download" className="mr-3" disabled={state.loading || noText} onClick={onDownload}>
-              <DownloadIcon className="mr-2 w-4 h-4" aria-hidden="true" /> Download to File
+          <CustomFormatInput
+            show={state.format === 'custom'}
+            customFormat={state.customFormat}
+            onChange={onCustomFormatChange}
+          />
+          {!singleMode && <LimitRange limit={state.limit} max={state.totalRecords} onLimitChange={onLimitChange} />}
+          <Collapse in={!singleMode || state.format === 'custom'}>
+            <Button data-testid="btn-apply" type="submit" isLoading={state.loading}>
+              Apply
             </Button>
-            <CopyToClipboard text={state.text} onCopy={handleOnCopy}>
-              <Button data-testid="btn-copy" disabled={state.loading || noText}>
-                <DuplicateIcon className="mr-2 w-4 h-4" aria-hidden="true" /> Copy to Clipboard
-              </Button>
-            </CopyToClipboard>
+          </Collapse>
+        </Flex>
+      </form>
+      <HStack>
+        <Button
+          data-testid="btn-download"
+          isDisabled={state.loading || noText}
+          onClick={onDownload}
+          rightIcon={<DownloadIcon />}
+        >
+          Download to File
+        </Button>
+        <CopyToClipboard text={state.text} onCopy={handleOnCopy}>
+          <Button data-testid="btn-copy" isDisabled={state.loading || noText} rightIcon={<CopyIcon />}>
+            Copy to Clipboard
+          </Button>
+        </CopyToClipboard>
 
-            <Transition show={copied} as="span" className="ml-4 text-blue-500 text-sm">
-              Copied!
-            </Transition>
-          </div>
-          <textarea
-            aria-label="export text"
-            data-testid="textarea"
-            value={state.text}
-            rows={10}
-            readOnly={true}
-            className="block w-full bg-gray-100 border border-gray-300 focus:border-indigo-500 rounded-md shadow-sm box-border focus:ring-indigo-500 sm:text-sm"
-          ></textarea>
-        </div>
-      </div>
-    </Panel>
+        <Fade in={copied}>Copied!</Fade>
+      </HStack>
+      <Textarea aria-label="export text" value={state.text} rows={10} readOnly={true}></Textarea>
+    </Flex>
   );
 };
 Export.propTypes = propTypes;
@@ -141,8 +135,8 @@ const CustomFormatInput = ({
     }
   }, [show, ref.current]);
   return (
-    <Transition show={show}>
+    <Collapse in={show}>
       <TextInput label="Custom Format" ref={ref} onChange={handleChange} value={customFormat} />
-    </Transition>
+    </Collapse>
   );
 };
