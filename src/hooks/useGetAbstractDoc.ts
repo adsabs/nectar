bafe2621ebaeb57ee328d369e47e5460cd6e122c
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-02-08 06:14:13 -0500

    Updates to abs/citations

---
+++ src/hooks/useGetAbstractDoc.ts
@@ -0,0 +1,15 @@
+import { IDocsEntity } from '@api';
+import { useGetAbstract } from '@_api/search';
+
+/**
+ * helper hook for getting hold of the primary doc
+ */
+export const useGetAbstractDoc = (id: string): IDocsEntity => {
+  // this *should* only ever fetch from pre-filled cache
+  const { data, isSuccess } = useGetAbstract({ id });
+
+  // should be able to access docs here directly
+  const doc = isSuccess ? data.docs?.[0] : undefined;
+
+  return doc;
+};
