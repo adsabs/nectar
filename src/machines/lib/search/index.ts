ba753afdbeaa4cb7f9c5e7d4950ee45e3bfeaafd
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-02-08 05:13:30 -0500

    Remove machines

--- src/machines/lib/search/index.ts
+++
@@ -1,4 +0,0 @@
-export { searchMachine } from './searchMachine';
-export { TransitionType as SearchMachineTransitionTypes } from './types';
-export type { Context as SearchMachineContext, Transition as SearchMachineTransition } from './types';
-export { useSearchMachine } from './useSearchMachine';
