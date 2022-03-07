d42db1f0159431b5c0e237b741a38efc2af6ba24
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-03-07 08:04:59 -0500

    Update abstract result pages with pagination

--- src/components/AbstractRefList/AbstractRefList.tsx
+++ src/components/AbstractRefList/AbstractRefList.tsx
@@ -2,18 +2,28 @@ import { IDocsEntity } from '@api';
 import { Link, Stack } from '@chakra-ui/layout';
 import { ISimpleResultListProps, SimpleResultList } from '@components';
 import { IPaginationProps, Pagination } from '@components/ResultList/Pagination';
+import { usePagination } from '@components/ResultList/Pagination/usePagination';
+import { useStore } from '@store';
 import NextLink, { LinkProps } from 'next/link';
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
 export interface IAbstractRefListProps {
   docs: IDocsEntity[];
   indexStart: ISimpleResultListProps['indexStart'];
   href: LinkProps['href'];
   totalResults: IPaginationProps['totalResults'];
-  onPageChange: IPaginationProps['onPageChange'];
+  onPageChange: (page: number, start: number) => void;
 }
 
 export const AbstractRefList = (props: IAbstractRefListProps): ReactElement => {
-  const { docs, href, totalResults, onPageChange, indexStart } = props;
+  const { docs, onPageChange, href, totalResults, indexStart } = props;
+  const storePagination = useStore((state) => state.pagination);
+  const pagination = usePagination({ numFound: totalResults, ...storePagination });
+
+  useEffect(() => {
+    if (typeof onPageChange === 'function') {
+      onPageChange(pagination.page, pagination.startIndex);
+    }
+  }, [pagination.page, pagination.startIndex]);
 
   return (
     <Stack direction="column" spacing={1} mt={1} w="full">
@@ -21,7 +31,7 @@ export const AbstractRefList = (props: IAbstractRefListProps): ReactElement => {
         <Link>View as search results</Link>
       </NextLink>
       <SimpleResultList docs={docs} hideCheckboxes={true} indexStart={indexStart} />
-      <Pagination totalResults={totalResults} numPerPage={10} onPageChange={onPageChange} />
+      <Pagination totalResults={totalResults} hidePerPageSelect {...pagination} />
     </Stack>
   );
 };
