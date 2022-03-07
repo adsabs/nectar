4f81ea2393b272a662e30f953a5739906b586854
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-03-07 08:01:08 -0500

    Complete refactor of usePagination hook
    Hook now just wraps the main logic, and is stateless

--- src/components/ResultList/Pagination/usePagination.ts
+++ src/components/ResultList/Pagination/usePagination.ts
@@ -1,150 +1,107 @@
 import { APP_DEFAULTS } from '@config';
-import { noop, useBaseRouterPath } from '@utils';
-import { useRouter } from 'next/router';
-import { ParsedUrlQuery } from 'querystring';
-import { clamp, range } from 'ramda';
-import { MouseEvent, MouseEventHandler, useCallback, useEffect, useMemo } from 'react';
-import { UrlObject } from 'url';
-export interface IUsePagination {
-  nextHref: UrlObject;
-  prevHref: UrlObject;
-  pages: { index: number; href: UrlObject }[];
-  startIndex: number;
-  endIndex: number;
-  page: number;
-  noPrev: boolean;
-  noNext: boolean;
-  noPagination: boolean;
-  totalPages: number;
-  handleNext: MouseEventHandler<HTMLButtonElement>;
-  handlePrev: MouseEventHandler<HTMLButtonElement>;
-  handlePageChange: (e: MouseEvent<HTMLButtonElement>, page: number) => void;
-}
-
-export interface IUsePaginationProps {
-  totalResults: number;
-  numPerPage: number;
-  onPageChange?: (page: number, start: number) => void;
-}
+import { clamp } from 'ramda';
+import { useMemo } from 'react';
 
 /**
- *
- */
-const parsePageFromQuery = (query: ParsedUrlQuery): number => {
-  try {
-    const { p } = query;
-    const page = parseInt(Array.isArray(p) ? p[0] : p, 10);
-    return page === 0 || Number.isNaN(page) ? 1 : page;
-  } catch (e) {
-    return 1;
-  }
-};
-
-/**
- *
+ * Calculate the total pages based on the number of results and how many records per page
  */
 const getTotalPages = (totalResults: number, numPerPage: number): number => {
   try {
-    const pages = Math.ceil(totalResults / clamp(1, 500, numPerPage));
-    return pages === 0 ? 1 : pages;
+    const pages = Math.ceil(totalResults / numPerPage);
+    return pages <= 0 ? 1 : pages;
   } catch (e) {
     return 1;
   }
 };
 
-export const usePagination = ({
-  totalResults = 1,
-  numPerPage = APP_DEFAULTS.RESULT_PER_PAGE,
-  onPageChange = noop,
-}: IUsePaginationProps): IUsePagination => {
-  const { query } = useRouter();
-  const { basePath } = useBaseRouterPath();
-
-  const state = useMemo(() => {
-    let page = parsePageFromQuery(query);
-    const totalPages = getTotalPages(totalResults, numPerPage);
-
-    // make sure page cannot surpass totalPages
-    if (page > totalPages) {
-      page = totalPages;
+/**
+ * Will convert the value from string to number and clamp the value between min and max
+ */
+const cleanClamp = (value: unknown, min: number = 0, max: number = Number.MAX_SAFE_INTEGER): number => {
+  try {
+    if (typeof value === 'number' && value >= min) {
+      return clamp(min, max, value);
+    } else if (typeof value === 'string') {
+      return clamp(min, max, Math.abs(parseInt(value, 10)));
     }
+    return min;
+  } catch (e) {
+    return min;
+  }
+};
 
-    // spreads the pagination control out, moving the current to the middle after the first few pages
-    const pageRange = range(
-      page <= 3 ? 1 : page - 3,
-      page < totalPages - 3 ? page + 4 : page + (totalPages - page + 1),
-    );
-    const startIndex = (page - 1) * numPerPage + 1;
-    const endIndex = startIndex + numPerPage - 1;
+/**
+ * Main logic
+ *
+ * Based on numFound, page and numPerPage - calculate all the necessary metadata needed to
+ * properly display the pagination controls or to calculate the next state
+ */
+export const calculatePagination = ({ numFound, page, numPerPage }: IUsePaginationProps): IUsePaginationResult => {
+  const results = cleanClamp(numFound, 0);
+  const totalPages = getTotalPages(results, numPerPage);
 
-    return {
-      nextHref: { pathname: basePath, query: { ...query, p: clamp(1, totalPages, page + 1) } },
-      prevHref: { pathname: basePath, query: { ...query, p: clamp(1, totalPages, page - 1) } },
-      pages: pageRange.map((index) => ({
-        index,
-        href: { pathname: basePath, query: { ...query, p: index } },
-      })),
-      page,
-      startIndex,
-      endIndex,
-      noPrev: page === 1,
-      noNext: page === totalPages,
-      totalPages,
-      noPagination: totalPages === 1,
-    };
-  }, [totalResults, numPerPage, query, basePath]);
+  // have to do some special handling for the final page
+  let startIndex;
+  let endIndex;
+  if (page === totalPages) {
+    // for the final page, we can calculate directly from results
+    if (results % numPerPage === 0) {
+      startIndex = results - numPerPage + 1;
+    } else {
+      startIndex = results - (results % numPerPage) + 1;
+    }
+    endIndex = results;
+  } else {
+    startIndex = cleanClamp((page - 1) * numPerPage + 1, 1, results - numPerPage + 1);
+    endIndex = startIndex + numPerPage - 1;
+  }
 
-  const handleNext = useCallback(
-    (e: MouseEvent<HTMLButtonElement>) => {
-      e.preventDefault();
-      if (state.noNext) {
-        return;
-      }
-
-      const page = state.page + 1;
-      const start = (state.page - 1) * numPerPage;
-      onPageChange(page, start);
-    },
-    [state.page, onPageChange],
-  );
-
-  const handlePrev = useCallback(
-    (e: MouseEvent<HTMLButtonElement>) => {
-      e.preventDefault();
-      if (state.noPrev) {
-        return;
-      }
-
-      const page = state.page - 1;
-      const start = (state.page - 1) * numPerPage;
-      onPageChange(page, start);
-    },
-    [state.page, onPageChange],
-  );
-
-  const handlePageChange = useCallback(
-    (e: MouseEvent<HTMLButtonElement>, page: number) => {
-      e.preventDefault();
-
-      if (page === state.page) {
-        return;
-      }
-
-      const start = (page - 1) * numPerPage;
-      onPageChange(page, start);
-    },
-    [state.page, onPageChange],
-  );
-
-  useEffect(() => {
-    const start = (state.page - 1) * numPerPage;
-    onPageChange(state.page, start);
-  }, [state.page]);
+  // calculate new page based on startIndex and numPerPage
+  const newPage = cleanClamp(Math.floor(startIndex / numPerPage) + 1, 1, totalPages);
 
   return {
-    ...state,
-    handleNext,
-    handlePrev,
-    handlePageChange,
+    // utility
+    nextPage: cleanClamp(newPage + 1, 1, totalPages),
+    prevPage: cleanClamp(newPage - 1, 1, totalPages),
+    noPrev: newPage === 1,
+    noNext: newPage === totalPages,
+    noPagination: totalPages <= 1,
+
+    // meta
+    startIndex,
+    endIndex,
+    totalPages,
+    page: newPage,
   };
+};
+
+export interface IUsePaginationProps {
+  numFound: number;
+  numPerPage: typeof APP_DEFAULTS['PER_PAGE_OPTIONS'][number];
+  page: number;
+}
+
+export interface IUsePaginationResult {
+  nextPage: number;
+  prevPage: number;
+  startIndex: number;
+  endIndex: number;
+  page: number;
+  totalPages: number;
+  noPagination: boolean;
+  noNext: boolean;
+  noPrev: boolean;
+}
+
+/**
+ * Pagination hook
+ *
+ * Basically wraps the pagination logic, also uses some memoization to reduce unnecessary renders.
+ */
+export const usePagination = (props: IUsePaginationProps): IUsePaginationResult => {
+  const { numFound = 0, numPerPage = APP_DEFAULTS.RESULT_PER_PAGE, page = 1 } = props;
+
+  const result = useMemo(() => calculatePagination({ numFound, numPerPage, page }), [numFound, numPerPage, page]);
+
+  return result;
 };
