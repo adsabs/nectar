2a2a1cbe61e35f25e85e0374f62a6decf999bedb
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-03-17 07:22:13 -0400

    lots of improvements to pagination/popstate for search

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,6 +1,6 @@
 import { IADSApiSearchParams, SolrSort } from '@api';
 import { Box, Flex } from '@chakra-ui/layout';
-import { Alert, AlertIcon } from '@chakra-ui/react';
+import { Alert, AlertIcon, useToast } from '@chakra-ui/react';
 import { VisuallyHidden } from '@chakra-ui/visually-hidden';
 import { ItemsSkeleton, ListActions, NumFound, SearchBar, SimpleResultList } from '@components';
 import { Pagination } from '@components/ResultList/Pagination';
@@ -13,33 +13,39 @@ import axios from 'axios';
 import { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { equals, omit, pick } from 'ramda';
-import { ChangeEvent, useEffect, useRef, useState } from 'react';
+import qs from 'qs';
+import { omit } from 'ramda';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { dehydrate, QueryClient } from 'react-query';
+import { useDebouncedCallback } from 'use-debounce';
 
 interface ISearchPageProps {
   searchParams: IADSApiSearchParams;
 }
 
-const useSearchQuery = (submitted: boolean, query: IADSApiSearchParams) => {
-  const router = useRouter();
-  const setLatestQuery = useStore((state) => state.setLatestQuery);
-  const setDocs = useStore((state) => state.setDocs);
+// selectors
+const updateQuerySelector = (state: AppState) => state.updateQuery;
+const setLatestQuerySelector = (state: AppState) => state.setLatestQuery;
+const setDocsSelector = (state: AppState) => state.setDocs;
+
+const useSearchQuery = () => {
+  const toast = useToast();
+  const store = useStoreApi();
+  const setLatestQuery = useStore(setLatestQuerySelector);
+  const setDocs = useStore(setDocsSelector);
+  const [query, setQuery] = useState(() => store.getState().query);
+
+  // debounce the onSubmit handler, so it can make sure store is fully updated prior to submission
+  const onSubmit = useDebouncedCallback(() => setQuery(store.getState().query), 50, {
+    trailing: true,
+    leading: false,
+    maxWait: 1000,
+  });
 
   const result = useSearch(query, {
     // we are allowing data to persist, but below the page will still show loading state
     // this is to keep stable data available for other hooks to use
     keepPreviousData: true,
-    enabled: submitted,
-    onSettled: () => {
-      // omit superfluous params
-      const params = omit(['fl', 'start', 'rows'], query);
-
-      // update router with changed params
-      void router.push({ pathname: router.pathname, query: { ...router.query, ...params } }, null, {
-        shallow: true,
-      });
-    },
     onSuccess(data) {
       // set docs on success
       setDocs(data.docs.map((d) => d.bibcode));
@@ -47,60 +53,67 @@ const useSearchQuery = (submitted: boolean, query: IADSApiSearchParams) => {
       // update store with the latest (working) query
       setLatestQuery(query);
     },
+    onError(error) {
+      if (error instanceof Error) {
+        toast({
+          title: 'Problem with search',
+          description: error.message,
+          status: 'error',
+        });
+      }
+    },
   });
 
-  return result;
+  return { ...result, query, onSubmit };
 };
 
 const SearchPage: NextPage<ISearchPageProps> = () => {
   const store = useStoreApi();
-  const updateQuery = useStore((state) => state.updateQuery);
-  const query = useStoreApi().getState().query;
-  const notInitialRender = useRef(false);
+  const updateQuery = useStore(updateQuerySelector);
+  const { data, isSuccess, isError, isLoading, error, query, onSubmit } = useSearchQuery();
 
-  const [submitted, setSubmitted] = useState(false);
-  const { data, isSuccess, isError, isFetching, error } = useSearchQuery(submitted, query);
+  useEffect(() => {
+    // omit superfluous params
+    const params = omit(['fl', 'start', 'rows'], query);
 
-  // helper for submitting and updating query at the same time
-  const updateAndSubmit = (params?: Partial<IADSApiSearchParams>) => {
-    if (params) {
-      updateQuery(params);
+    // update router with changed params
+    void router.push({ pathname: router.pathname, query: { ...router.query, ...params } }, null, {
+      shallow: true,
+    });
+
+    // look at current query value, and if page is there update pagination to make sure it stays in sync with URL
+    // if we're resetting the pagination anyway don't bother dispatching here
+    if (typeof router.query.p === 'string' && query.start > 0) {
+      pagination.dispatch({ type: 'SET_PAGE', payload: parseNumberAndClamp(router.query.p, 1) });
     }
-    setSubmitted(true);
-    setTimeout(() => setSubmitted(false), 0);
-  };
+  }, [query]);
 
   // re-calculates pagination when numFound, page or numPerPage changes
   const pagination = usePagination({ numFound: data?.numFound });
 
   // on pagination updates we want to attempt another search
   useEffect(() => {
-    updateAndSubmit({ start: pagination.startIndex, rows: pagination.numPerPage });
+    updateQuery({ start: pagination.startIndex, rows: pagination.numPerPage });
+    onSubmit();
   }, [pagination.startIndex, pagination.numPerPage]);
 
-  // update the page and query based on the incoming route params
+  // on popstate change, trigger a new search (back button pressed)
   const router = useRouter();
-  const routeChangeHandler = () => {
-    const page = parseNumberAndClamp(router.query.p, 1);
-    if (page !== pagination.page) {
-      pagination.dispatch({ type: 'SET_PAGE', payload: page });
-    }
-
-    // only do updates when something has actually changed, since this handler
-    // will call on all updates to the URL
-    const parsedQuery = parseQueryFromUrl(router.query);
-    if (!equals(pick(['q', 'sort'], parsedQuery), pick(['q', 'sort'], query))) {
-      updateAndSubmit({ ...omit(['p'], router.query) });
-    }
-  };
-
-  // add/remove route change handlers
   useEffect(() => {
-    router.events.on('routeChangeComplete', routeChangeHandler);
-    return () => router.events.off('routeChangeComplete', routeChangeHandler);
-  }, [router]);
+    router.beforePopState(({ as }) => {
+      try {
+        const params = parseQueryFromUrl(qs.parse(as.split('?')?.[1]));
+        updateQuery(omit(['p'], params));
+        pagination.dispatch({ type: 'SET_PAGE', payload: params?.p });
+      } catch (e) {
+        // do nothing
+      } finally {
+        return true;
+      }
+    });
 
-  // TODO: pagination and updateAndSubmit compete for updating the query, figure out better solution
+    return () => router.beforePopState(() => true);
+  }, [router]);
 
   // on form submission, but not otherwise (probably need more granular control)
   // we should clear the selected docs.  We'll probably want to not do this ordinal changes like sort
@@ -109,10 +122,8 @@ const SearchPage: NextPage<ISearchPageProps> = () => {
     e.preventDefault();
     const { q } = store.getState().query;
     if (q && q.trim().length > 0) {
-      updateAndSubmit();
-
-      // resets the page to 1
       pagination.dispatch({ type: 'RESET' });
+      onSubmit();
       clearSelectedDocs();
     }
   };
@@ -120,13 +131,10 @@ const SearchPage: NextPage<ISearchPageProps> = () => {
   // trigger new search on sort change
   // should also reset pagination, but not clear docs
   const handleSortChange = (sort: SolrSort[]) => {
-    updateAndSubmit({ sort });
-
-    // resets the page to 1
+    updateQuery({ sort });
     pagination.dispatch({ type: 'RESET' });
+    onSubmit();
   };
-
-  const isLoading = isFetching || submitted;
 
   return (
     <Box aria-labelledby="search-form-title" my={16}>
@@ -168,6 +176,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const params: IADSApiSearchParams = {
     ...defaultParams,
     ...query,
+    q: query.q === '' ? '*:*' : query.q,
     start: (page - 1) * defaultParams.rows,
   };
 
