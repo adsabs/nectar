e0bacec38bfb2991899ced2b9d234aee5f596332
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-03-18 04:51:26 -0400

    Remove unnecessary getInitialProps

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,15 +2,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { ColorModeScript } from '@chakra-ui/react';
 import { theme } from '@theme';
-import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ReactElement } from 'react';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render(): ReactElement {
     return (
       <Html lang="en">
