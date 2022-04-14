5a3625fdd3bed112f795ba20b17ef08c35cce49f
Jennifer Chen <shinyi.chen@gmail.com>
2022-04-14 11:48:16 -0400

    Prefetch some data during SSR

--- src/pages/search/metrics.tsx
+++ src/pages/search/metrics.tsx
@@ -1,60 +1,93 @@
+import { Alert, AlertDescription, AlertIcon, AlertTitle } from '@chakra-ui/react';
 import { VizPageLayout } from '@components';
 import { MetricsPageContainer } from '@components';
-import { parseQueryFromUrl } from '@utils';
-import { useSearch } from '@_api/search';
+import { parseQueryFromUrlNoPage } from '@utils';
+import { searchKeys } from '@_api/search';
 import { getSearchParams } from '@_api/search/models';
-import { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { GetServerSideProps, NextPage } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import { dehydrate, DehydratedState, QueryClient } from 'react-query';
 
-const MetricsPage: NextPage = () => {
-  const router = useRouter();
+const limit = 7000;
+interface IMetricsProps {
+  query?: ParsedUrlQuery;
+  qid?: string;
+  numFound?: number;
+  dehydratedState?: DehydratedState;
+  error?: string;
+}
 
-  const limit = 7000;
-
-  const [recordsToGet, setRecordsToGet] = useState(0);
-
-  const { qid, p, ...query } = router.query;
-
-  // query to get docs count from original query
-  const { data: countByQuery, refetch: fetchRecCountByQuery } = useSearch(
-    getSearchParams({ ...parseQueryFromUrl(query), rows: 1 }),
-    {
-      enabled: false,
-    },
-  );
-
-  // query to get docs count from vault (qid)
-  const { data: countByQid, refetch: fetchRecCountByQid } = useSearch(
-    getSearchParams({ q: `docs(${qid as string})` }),
-    {
-      enabled: false,
-    },
-  );
-
-  // new query
-  useEffect(() => {
-    if (qid) {
-      // if has qid, use qid to get set of bibcodes
-      void fetchRecCountByQid();
-    } else if (query.q) {
-      // if no qid, use query to get set of bibcodes
-      void fetchRecCountByQuery();
-    }
-  }, [router]);
-
-  useEffect(() => {
-    const numFound = qid ? countByQid?.numFound : countByQuery?.numFound;
-    if (typeof numFound === 'number') {
-      setRecordsToGet(Math.min(numFound, limit));
-    }
-  }, [countByQuery, countByQid]);
-
+const MetricsPage: NextPage<IMetricsProps> = (props) => {
+  const { query, qid, numFound, error } = props;
   return (
-    <VizPageLayout vizPage="metrics" from={{ pathname: '/search', query: { ...query, p } }}>
-      <MetricsPageContainer query={query} qid={qid as string} recordsToGet={recordsToGet} />
+    <VizPageLayout vizPage="metrics" from={{ pathname: '/search', query: query }}>
+      {error ? (
+        <Alert status="error" my={5}>
+          <AlertIcon />
+          <AlertTitle mr={2}>Error fetching records!</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      ) : (
+        <MetricsPageContainer query={query} qid={qid} recordsToGet={numFound} />
+      )}
     </VizPageLayout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<IMetricsProps> = async (ctx) => {
+  const api = (await import('@_api/api')).default;
+  const { fetchSearch } = await import('@_api/search');
+  const { qid, ...query } = ctx.query;
+  api.setToken(ctx.req.session.userData.access_token);
+  const queryClient = new QueryClient();
+
+  try {
+    // get numbers of records count for this query
+    const params = qid
+      ? getSearchParams({ q: `docs(${qid as string})` })
+      : getSearchParams({ ...parseQueryFromUrlNoPage(query) });
+    const numFoundRes = await queryClient.fetchQuery({
+      queryKey: searchKeys.primary(params),
+      queryFn: fetchSearch,
+      meta: { params },
+      retry: false,
+    });
+
+    const numFound = Math.min(numFoundRes.response?.numFound ?? 0, limit);
+
+    // prefetch the list of bibcodes for this query
+    let remainsToFetch = numFound;
+    let start = 0;
+    while (remainsToFetch > 0) {
+      const p = qid
+        ? { q: `docs(${qid as string})`, start: start, rows: 1000, fl: ['bibcode'] }
+        : { ...parseQueryFromUrlNoPage(query), start: start, rows: 1000, fl: ['bibcode'] };
+      await queryClient.prefetchQuery({
+        queryKey: ['search/bibcodes', p],
+        queryFn: fetchSearch,
+        meta: { params: p },
+        retry: false,
+      });
+      start += 1000;
+      remainsToFetch -= 1000;
+    }
+
+    return {
+      props: {
+        query,
+        qid: qid ? (qid as string) : null,
+        numFound,
+        dehydratedState: dehydrate(queryClient),
+      },
+    };
+  } catch (e) {
+    return {
+      props: {
+        error: axios.isAxiosError(e) ? e.message : 'Unable to fetch data',
+      },
+    };
+  }
 };
 
 export default MetricsPage;
