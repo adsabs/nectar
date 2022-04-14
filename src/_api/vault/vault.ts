db95ba302024a9490a14b2bf1df5104cc1f2043e
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-04-14 18:26:17 -0400

    Remove bigquery, add vault endpoint

---
+++ src/_api/vault/vault.ts
@@ -0,0 +1,77 @@
+import { IADSApiSearchParams, IADSApiSearchResponse, IDocsEntity } from '@api';
+import { ApiTargets } from '@api/lib/models';
+import api, { ApiRequestConfig } from '@_api/api';
+import { ADSQuery } from '@_api/types';
+import { QueryFunction, useQuery } from 'react-query';
+import { getVaultBigQueryParams } from './models';
+import { IADSApiVaultResponse, IADSVaultExecuteQueryParams } from './types';
+
+export const vaultKeys = {
+  primary: (params: IADSApiSearchParams) => ['vault', { params }] as const,
+  executeQuery: (qid: IADSVaultExecuteQueryParams['qid']) => ['vault/execute_query', { qid }] as const,
+  bigquery: (bibcodes: IDocsEntity['bibcode'][]) => ['vault/bigquery', { bibcodes }] as const,
+};
+
+/**
+ * Generic search, allows for any solr query params
+ * Vault will save this query and return a QID for use in other searches/filters/etc.
+ */
+export const useVaultSearch: ADSQuery<IADSApiSearchParams, IADSApiVaultResponse> = (params, options) => {
+  return useQuery({
+    queryKey: vaultKeys.primary(params),
+    queryFn: fetchVaultSearch,
+    meta: { params },
+    ...options,
+  });
+};
+
+/**
+ * Request to vault to retreive the cached query by QID
+ */
+export const useVaultExecuteQuery: ADSQuery<IADSVaultExecuteQueryParams, IADSApiVaultResponse> = ({ qid }, options) => {
+  const params = { qid };
+  return useQuery({
+    queryKey: vaultKeys.executeQuery(qid),
+    queryFn: fetchVaultSearch,
+    meta: { params },
+    ...options,
+  });
+};
+
+/**
+ * Request to vault to save a bigquery search (for set of bibcodes) with sensible defaults
+ */
+export const useVaultBigQuerySearch: ADSQuery<IDocsEntity['bibcode'][], IADSApiVaultResponse> = (bibcodes, options) => {
+  const params = getVaultBigQueryParams(bibcodes);
+  return useQuery({
+    queryKey: vaultKeys.bigquery(bibcodes),
+    queryFn: fetchVaultSearch,
+    meta: { params },
+    ...options,
+  });
+};
+
+export const fetchVaultSearch: QueryFunction<IADSApiVaultResponse> = async ({ meta }) => {
+  const { params } = meta as { params: IADSApiSearchParams };
+
+  const config: ApiRequestConfig = {
+    method: 'POST',
+    url: `${ApiTargets.MYADS_STORAGE}/query`,
+    params,
+  };
+
+  const { data } = await api.request<IADSApiVaultResponse>(config);
+  return data;
+};
+
+export const fetchVaultExecuteQuery: QueryFunction<IADSApiSearchResponse['response']> = async ({ meta }) => {
+  const { params } = meta as { params: IADSVaultExecuteQueryParams };
+
+  const config: ApiRequestConfig = {
+    method: 'GET',
+    url: `${ApiTargets.MYADS_STORAGE}/execute_query/${params.qid}`,
+  };
+
+  const { data } = await api.request<IADSApiSearchResponse['response']>(config);
+  return data;
+};
