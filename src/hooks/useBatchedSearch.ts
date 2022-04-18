995e74dccabd8e7c1da30ff8416645eb0dedf72c
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-04-18 00:31:27 -0400

    Add batched request hook

---
+++ src/hooks/useBatchedSearch.ts
@@ -0,0 +1,53 @@
+import { IADSApiSearchParams, IADSApiSearchResponse } from '@api';
+import { useSearchInfinite } from '@_api/search';
+import { AxiosError } from 'axios';
+import { chain } from 'ramda';
+import { useEffect, useRef } from 'react';
+import { UseInfiniteQueryOptions } from 'react-query';
+
+const DELAY_BETWEEN_REQUESTS = 500;
+
+export interface IUseBatchedSearchProps {
+  batches: number;
+  transform?: (res: IADSApiSearchResponse) => unknown[];
+}
+
+/**
+ * Hook to get search results in batches (by rows)
+ */
+export const useBatchedSearch = (
+  params: IADSApiSearchParams,
+  props: IUseBatchedSearchProps,
+  options?: UseInfiniteQueryOptions<IADSApiSearchResponse & { pageParam: string }, Error | AxiosError>,
+) => {
+  const { batches, transform = (res) => res.response.docs } = props;
+
+  const { data, isFetchingNextPage, fetchNextPage, fetchPreviousPage, isFetchingPreviousPage, ...rest } =
+    useSearchInfinite(params, {
+      onSuccess: () => (count.current -= 1),
+      ...options,
+    });
+
+  const count = useRef(batches);
+  useEffect(() => {
+    if (count.current === batches) {
+      // first run
+      void fetchNextPage();
+    }
+
+    // internal fetch loop, delay between requests
+    if (!isFetchingNextPage && count.current > 0) {
+      setTimeout(() => void fetchNextPage(), DELAY_BETWEEN_REQUESTS);
+    }
+  }, [isFetchingNextPage, batches]);
+
+  // determine if we should notify consumers yet
+  if (count.current === 0) {
+    // flatMap over the pages running our transformer
+    const docs = chain(transform, data.pages);
+    return { data: { docs, numFound: docs.length }, ...rest };
+  }
+
+  // if we're not ready yet, data should be undefined
+  return { data: undefined, ...rest };
+};
