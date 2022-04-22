e8ad89dba1fc2300dfe6d4f2fe8de38cd5e3a573
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-04-22 19:04:31 -0400

    add simple test for hook

---
+++ src/hooks/__tests__/useBatchedSearch.tsx
@@ -0,0 +1,38 @@
+import { useBatchedSearch } from '@hooks/useBatchedSearch';
+import { renderHook } from '@testing-library/react-hooks';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+const setup = (initialProps: Parameters<typeof useBatchedSearch>) => {
+  const utils = renderHook<Parameters<typeof useBatchedSearch>, ReturnType<typeof useBatchedSearch>>(
+    (args) => useBatchedSearch(...args),
+    {
+      initialProps,
+      wrapper: ({ children }) => {
+        return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+      },
+    },
+  );
+
+  return utils;
+};
+
+describe('useBatchedSearch', () => {
+  test('basic case works without crashing', async () => {
+    const batches = 5;
+    const rows = 10;
+    const { result, waitFor } = setup([
+      { q: 'star', rows, fl: ['bibcode'] },
+      { batches, intervalDelay: 1 },
+    ]);
+    await waitFor(() => !!result.current.data, { timeout: 3000 });
+    expect(result.current.data.numFound).toEqual(batches * rows);
+    expect(result.current.data.docs).toHaveLength(batches * rows);
+  });
+});
