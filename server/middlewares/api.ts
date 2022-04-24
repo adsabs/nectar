cfe72f9099e93a0f6cfd53c54821d6070e6e0a7c
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-04-24 02:16:54 -0400

    update server-side bootstrapper

--- server/middlewares/api.ts
+++ server/middlewares/api.ts
@@ -1,6 +1,48 @@
+import axios, { AxiosResponse } from 'axios';
+import { isPast, parseISO } from 'date-fns';
 import { RequestHandler as Middleware } from 'express';
-import Adsapi from '../../src/api';
-import { checkUserData } from '../../src/api/lib/utils';
+import getConfig from 'next/config';
+import { isNil } from 'ramda';
+import { ApiTargets, IBootstrapPayload, IUserData } from '../../src/api';
+
+const isUserData = (userData?: IUserData): userData is IUserData => {
+  return (
+    !isNil(userData) &&
+    typeof userData.access_token === 'string' &&
+    typeof userData.expire_in === 'string' &&
+    userData.access_token.length > 0 &&
+    userData.expire_in.length > 0
+  );
+};
+
+export const checkUserData = (userData?: IUserData): boolean => {
+  return isUserData(userData) && !isPast(parseISO(userData.expire_in));
+};
+
+/**
+ * Figure out which config to pick, based on the current environment
+ */
+const resolveApiBaseUrl = (defaultBaseUrl = ''): string => {
+  // for mocking requests, just shortcut the baseURL here
+  if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled' && process.env.NODE_ENV !== 'production') {
+    return '/';
+  }
+
+  const config = getConfig();
+
+  if (typeof config === 'undefined') {
+    return defaultBaseUrl;
+  }
+
+  const configType = typeof window === 'undefined' ? 'serverRuntimeConfig' : 'publicRuntimeConfig';
+  return config[configType]?.apiHost ?? defaultBaseUrl;
+};
+
+interface IADSApiBootstrapResponse extends AxiosResponse<IBootstrapPayload> {
+  headers: {
+    'set-cookie': string;
+  };
+}
 
 export const api: Middleware = (req, res, next) => {
   // grab reference to our current session from the request
@@ -11,21 +53,18 @@ export const api: Middleware = (req, res, next) => {
   if (checkUserData(currentUserData)) {
     return next();
   }
-  const api = new Adsapi();
 
-  api.accounts
-    .bootstrap()
-    .then((bootstrapResponse) => {
-      if (bootstrapResponse.isOk()) {
-        const { data, headers } = bootstrapResponse.value;
-
-        // put the userData in the session
-        session.userData = data;
-
-        // should have recieved a session cookie, pass that in the response
-        res.setHeader('set-cookie', headers['set-cookie']);
-      }
-      next();
+  axios
+    .get<IBootstrapPayload, IADSApiBootstrapResponse>(ApiTargets.BOOTSTRAP, {
+      baseURL: resolveApiBaseUrl(),
     })
-    .catch(() => next());
+    .then((response) => {
+      const { data, headers } = response;
+
+      session.userData = data;
+      res.setHeader('set-cookie', headers['set-cookie']);
+    })
+    .catch(() => {
+      next();
+    });
 };
