a2247d36c0d0870bb3d0c0cdb4e0ca57b7c7ca12
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-04-24 01:35:27 -0400

    add/update barrel files

---
+++ src/api/index.ts
@@ -0,0 +1,7 @@
+export * from './accounts';
+export * from './biblib';
+export * from './export';
+export * from './graphics';
+export * from './metrics';
+export * from './reference';
+export * from './search';
--- src/api/search/index.ts
+++ src/api/search/index.ts
@@ -1,1 +1,3 @@
+export * from './models';
 export * from './search';
+export * from './types';
