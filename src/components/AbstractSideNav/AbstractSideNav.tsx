aa98ccc9775e93735d23f5ab2830ec0b1f7675f5
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-05-06 02:24:06 -0400

    refactor abstractSideNav

--- src/components/AbstractSideNav/AbstractSideNav.tsx
+++ src/components/AbstractSideNav/AbstractSideNav.tsx
@@ -5,206 +5,306 @@ import { Badge, Box, Flex, Link, Stack, Text } from '@chakra-ui/layout';
 import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/menu';
 import { SimpleLinkList } from '@components';
 import { ItemType } from '@components/Dropdown/types';
-import { DocumentIcon } from '@heroicons/react/outline';
+import {
+  ChartPieIcon,
+  ClipboardListIcon,
+  CollectionIcon,
+  DocumentIcon,
+  DocumentTextIcon,
+  DownloadIcon,
+  DuplicateIcon,
+  PhotographIcon,
+  TableIcon,
+  UsersIcon,
+} from '@heroicons/react/solid';
 import { useIsClient } from '@hooks/useIsClient';
-import { useBaseRouterPath } from '@utils';
-import NextLink from 'next/link';
+import { noop, parseQueryFromUrl } from '@utils';
+import NextLink, { LinkProps } from 'next/link';
 import { useRouter } from 'next/router';
-import qs from 'qs';
-import { last } from 'ramda';
-import { HTMLAttributes, ReactElement } from 'react';
-import { navigation, Routes } from './model';
+import { cloneElement, HTMLAttributes, ReactElement } from 'react';
+import { Routes } from './model';
 
+const useGetItems = ({
+  doc,
+  hasMetrics,
+  hasGraphics,
+}: {
+  doc: IDocsEntity;
+  hasMetrics: boolean;
+  hasGraphics: boolean;
+}) => {
+  const router = useRouter();
+
+  const items: Record<
+    Routes,
+    {
+      route: string;
+      label: string;
+      icon: ReactElement;
+      count?: number;
+      disabled?: boolean;
+      linkProps?: (id: string) => LinkProps;
+      active?: boolean;
+    }
+  > = {
+    [Routes.ABSTRACT]: {
+      route: Routes.ABSTRACT,
+      label: 'Abstract',
+      icon: <DocumentTextIcon />,
+    },
+    [Routes.CITATIONS]: {
+      route: Routes.CITATIONS,
+      label: 'Citations',
+      icon: <CollectionIcon />,
+      count: doc?.citation_count ?? 0,
+    },
+    [Routes.REFERENCES]: {
+      route: Routes.REFERENCES,
+      label: 'References',
+      icon: <ClipboardListIcon />,
+      count: doc['[citations]']?.num_references ?? 0,
+    },
+    [Routes.COREADS]: {
+      route: Routes.COREADS,
+      label: 'Co-Reads',
+      icon: <UsersIcon />,
+      count: doc?.read_count ?? 0,
+    },
+    [Routes.SIMILAR]: {
+      route: Routes.SIMILAR,
+      label: 'Similar Papers',
+      icon: <DuplicateIcon />,
+      disabled: !doc?.abstract,
+    },
+    [Routes.VOLUMECONTENT]: {
+      route: Routes.VOLUMECONTENT,
+      label: 'Volume Content',
+      icon: <TableIcon />,
+      disabled: doc.property?.indexOf('TOC') > -1,
+    },
+    [Routes.GRAPHICS]: {
+      route: Routes.GRAPHICS,
+      label: 'Graphics',
+      icon: <PhotographIcon />,
+      disabled: !hasGraphics,
+    },
+    [Routes.METRICS]: {
+      route: Routes.METRICS,
+      label: 'Metrics',
+      icon: <ChartPieIcon />,
+      disabled: !hasMetrics,
+    },
+    [Routes.EXPORT]: {
+      route: Routes.EXPORT,
+      label: 'Export Citation',
+      icon: <DownloadIcon />,
+      linkProps: (docId) => ({
+        href: `/abs/[id]/${Routes.EXPORT}/[format]`,
+        as: `/abs/${docId}/${Routes.EXPORT}/bibtex`,
+      }),
+    },
+  };
+
+  return {
+    items,
+    activeItem: Object.keys(items).find((route) => router.asPath.indexOf(`/${route}`) > -1) as Routes,
+  };
+};
 export interface IAbstractSideNavProps extends HTMLAttributes<HTMLDivElement> {
   doc?: IDocsEntity;
 }
 
-export const AbstractSideNav = ({ doc }: IAbstractSideNavProps): ReactElement => {
-  const router = useRouter();
+export const AbstractSideNav = (props: IAbstractSideNavProps): ReactElement => {
   const isClient = useIsClient();
-  const { basePath } = useBaseRouterPath();
-  const subPage = last(basePath.split('/'));
+  return isClient ? <Component {...props} /> : <Static {...props} />;
+};
+
+const Component = (props: IAbstractSideNavProps): ReactElement => {
+  const { doc } = props;
   const hasGraphics = useHasGraphics(doc.bibcode);
   const hasMetrics = useHasMetrics(doc.bibcode);
-  const hasToc = doc.property ? doc.property.indexOf('TOC') > -1 : false;
-  const useCount = [Routes.CITATIONS, Routes.REFERENCES];
-
-  const items = navigation.map((item) => {
-    const MenuIcon = item.icon || DocumentIcon;
-    const current = item.href === subPage;
-    const count =
-      item.href === Routes.EXPORT ||
-      (item.href === Routes.GRAPHICS && (hasGraphics || !isClient)) ||
-      (item.href === Routes.METRICS && (hasMetrics || !isClient)) ||
-      (item.href === Routes.VOLUMECONTENT && hasToc)
-        ? 1
-        : getCount(item.href, doc);
-    const disabled = count === 0 && item.href !== Routes.ABSTRACT;
-    const showCount = count > 0 && useCount.includes(item.href);
-    const href = { pathname: disabled ? Routes.ABSTRACT : item.href, query: { id: router.query.id } };
-
-    const label = (
-      <Stack direction="row" alignItems="center">
-        <MenuIcon className="mr-3 w-6 h-6" aria-hidden="true" />
-        <Text fontWeight="normal">{item.name}</Text>
-        {showCount ? (
-          <Badge
-            mx={3}
-            py={1}
-            fontSize="xs"
-            fontWeight="normal"
-            borderRadius="xl"
-            colorScheme="gray"
-            px={2}
-            backgroundColor="gray.50"
-          >
-            {count}
-          </Badge>
-        ) : null}
-      </Stack>
-    );
-
-    return {
-      id: item.name,
-      name: item.name,
-      disabled,
-      current,
-      label,
-      href,
-      count,
-      icon: MenuIcon,
-      showCount,
-    };
-  });
-
-  const getTopMenu = () => {
-    const currentItem = items.find((item) => item.current) ?? items[0];
-
-    const { icon: MenuIcon, showCount, count } = currentItem;
-
-    const label = (
-      <Flex
-        alignItems="center"
-        justifyContent="space-between"
-        backgroundColor="gray.50"
-        borderRadius="md"
-        px={3}
-        py={2}
-        width="full"
-      >
-        <Flex direction="row" width="full">
-          <MenuIcon className="mr-3 w-6 h-6" aria-hidden="true" />
-          <Text>{currentItem.name}</Text>
-          {showCount ? (
-            <Badge
-              mx={3}
-              py={1}
-              fontSize="xs"
-              fontWeight="normal"
-              borderRadius="xl"
-              colorScheme="gray"
-              px={2}
-              backgroundColor="white"
-            >
-              {count}
-            </Badge>
-          ) : null}
-        </Flex>
-        <ChevronDownIcon />
-      </Flex>
-    );
-
-    const getSimpleItems = (): ItemType[] => {
-      const res = items.map((item) => ({
-        id: item.id,
-        label: item.label,
-        path: `${item.href.pathname}?${qs.stringify(item.href.query)}`,
-        disabled: item.disabled,
-      }));
-      return res;
-    };
-
-    return (
-      <>
-        {isClient ? (
-          <Menu matchWidth>
-            <MenuButton width="full">{label}</MenuButton>
-            <MenuList>
-              {items.map((item) => (
-                <MenuItem
-                  key={item.id}
-                  isDisabled={item.disabled}
-                  backgroundColor={item.current ? 'gray.100' : 'transparent'}
-                  mb={1}
-                >
-                  <NextLink key={item.name} href={item.href} passHref>
-                    <Box width="full">{item.label}</Box>
-                  </NextLink>
-                </MenuItem>
-              ))}
-            </MenuList>
-          </Menu>
-        ) : (
-          <>
-            {/* <SimpleLinkDropdown items={getSimpleItems()} label={label} minLabelWidth="full" minListWidth="full" /> */}
-            <SimpleLinkList
-              items={getSimpleItems()}
-              minWidth="full"
-              selected={currentItem.id}
-              label="Abstract Navigation"
-            />
-          </>
-        )}
-      </>
-    );
-  };
+  const { items, activeItem } = useGetItems({ doc, hasGraphics, hasMetrics });
 
   return (
     <>
+      {/* Large viewports */}
       <Box as="nav" aria-label="sidebar" display={{ base: 'none', lg: 'initial' }}>
         <Flex direction="column" alignItems="start" justifyContent="start" shadow="md" borderRadius="md" p={2}>
-          {items.map((item) => (
-            <NextLink key={item.name} href={item.href} passHref>
-              <Link variant="dropdownItem" w="full" tabIndex={-1}>
-                <Button
-                  variant={item.current ? 'solid' : 'ghost'}
-                  size="md"
-                  aria-current={item.current ? 'page' : undefined}
-                  isDisabled={item.disabled}
-                  width="full"
-                  justifyContent="start"
-                  colorScheme="gray"
-                  mb={1}
-                >
-                  {item.label}
-                </Button>
-              </Link>
-            </NextLink>
+          {Object.values(items).map((itemProps) => (
+            <Item key={itemProps.route} {...itemProps} />
           ))}
         </Flex>
       </Box>
+
+      {/* Small viewports */}
       <Box as="nav" display={{ base: 'initial', lg: 'none' }}>
-        {getTopMenu()}
+        <Menu matchWidth>
+          <MenuButton width="full">
+            <TopMenuButton {...items[activeItem]} />
+          </MenuButton>
+          <MenuList>
+            {Object.values(items).map((itemProps) => (
+              <TopMenuItem key={itemProps.route} {...itemProps} />
+            ))}
+          </MenuList>
+        </Menu>
       </Box>
     </>
   );
 };
 
-const getCount = (route: Routes, doc: IDocsEntity) => {
-  if (!doc) {
-    return 0;
+interface IItemProps {
+  disabled?: boolean;
+  label: string;
+  count?: number;
+  route: string;
+  icon: ReactElement;
+  linkProps?: (id: string) => LinkProps;
+}
+const Item = (props: IItemProps) => {
+  const { disabled = false, label, count, route, icon = <DocumentIcon />, linkProps = noop } = props;
+  const router = useRouter();
+
+  let isDisabled = disabled;
+  if (typeof count === 'number' && count <= 0) {
+    isDisabled = true;
   }
 
-  switch (route) {
-    case Routes.CITATIONS:
-      return typeof doc.citation_count === 'number' ? doc.citation_count : 0;
-    case Routes.REFERENCES:
-      return typeof doc['[citations]'].num_references === 'number' ? doc['[citations]'].num_references : 0;
-    case Routes.COREADS:
-      return typeof doc.read_count === 'number' ? doc.read_count : 0;
-    case Routes.SIMILAR:
-      return typeof doc.abstract !== 'undefined' ? 1 : 0;
-    default:
-      return 0;
+  const { id: docId } = parseQueryFromUrl<{ id: string }>(router.query);
+  const active = router.asPath.indexOf(`/${route}`) > -1;
+
+  return (
+    <NextLink href={`/abs/[id]/${route}`} as={`/abs/${docId}/${route}`} passHref {...linkProps(docId)}>
+      <Link variant="dropdownItem" w="full" tabIndex={-1}>
+        <Button
+          variant={active ? 'solid' : 'ghost'}
+          size="md"
+          aria-current={active ? 'page' : undefined}
+          isDisabled={isDisabled}
+          width="full"
+          justifyContent="start"
+          colorScheme="gray"
+          mb={1}
+          className="flex-shi"
+          leftIcon={cloneElement(icon, { className: 'w-6 h-6', ariaHidden: true })}
+        >
+          {label}
+        </Button>
+      </Link>
+    </NextLink>
+  );
+};
+
+const TopMenuItem = (props: IItemProps) => {
+  const { disabled = false, label, count, route, icon = <DocumentIcon />, linkProps = noop } = props;
+  const router = useRouter();
+
+  let isDisabled = disabled;
+  if (typeof count === 'number' && count <= 0) {
+    isDisabled = true;
   }
+
+  const active = router.asPath.indexOf(`/${route}`) > -1;
+  const { id: docId } = parseQueryFromUrl<{ id: string }>(router.query);
+
+  return (
+    <MenuItem isDisabled={isDisabled} backgroundColor={active ? 'gray.100' : 'transparent'} mb={1}>
+      <NextLink href={`/abs/[id]/${route}`} as={`/abs/${docId}/${route}`} passHref {...linkProps(docId)}>
+        <Box width="full">
+          <Stack direction="row" alignItems="center">
+            {cloneElement(icon, { className: 'mr-3 w-6 h-6', ariaHidden: true })}
+            <Text fontWeight="normal">{label}</Text>
+            <CountBadge count={count} />
+          </Stack>
+        </Box>
+      </NextLink>
+    </MenuItem>
+  );
+};
+
+const TopMenuButton = (props: IItemProps) => {
+  const { label, count, icon } = props;
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="space-between"
+      backgroundColor="gray.50"
+      borderRadius="md"
+      px={3}
+      py={2}
+      width="full"
+    >
+      <Flex direction="row" width="full">
+        {cloneElement(icon, { className: 'mr-3 w-6 h-6', ariaHidden: true })}
+        <Text>{label}</Text>
+        <CountBadge count={count} />
+      </Flex>
+      <ChevronDownIcon className="w-6 h-6" />
+    </Flex>
+  );
+};
+
+const CountBadge = ({ count }: { count: number }): ReactElement => {
+  if (typeof count !== 'number') {
+    return null;
+  }
+  return (
+    <Badge
+      mx={3}
+      py={1}
+      fontSize="xs"
+      fontWeight="normal"
+      borderRadius="xl"
+      colorScheme="gray"
+      px={2}
+      backgroundColor="gray.50"
+    >
+      {count}
+    </Badge>
+  );
+};
+
+const Static = (props: IAbstractSideNavProps) => {
+  const { doc } = props;
+  const hasGraphics = useHasGraphics(doc.bibcode);
+  const hasMetrics = useHasMetrics(doc.bibcode);
+  const { items, activeItem } = useGetItems({ doc, hasGraphics, hasMetrics });
+  const getSimpleItems = (): ItemType[] => {
+    const res = Object.values(items).map((item) => ({
+      id: item.route,
+      label: (
+        <>
+          <Stack direction="row" alignItems="center">
+            {cloneElement(item.icon, { className: 'mr-3 w-6 h-6', ariaHidden: true })}
+            <Text fontWeight="normal">{item.label}</Text>
+            <CountBadge count={item.count} />
+          </Stack>
+        </>
+      ),
+
+      // TODO: need to refactor this so it supports dynamic routes (i.e. linkProps)
+      path:
+        item.route === Routes.EXPORT ? `/abs/${doc.bibcode}/${item.route}/bibtex` : `/abs/${doc.bibcode}/${item.route}`,
+      disabled: item.disabled,
+    }));
+    return res;
+  };
+
+  return (
+    <>
+      {/* Large viewports */}
+      <Box as="nav" aria-label="sidebar" display={{ base: 'none', lg: 'initial' }}>
+        <Flex direction="column" alignItems="start" justifyContent="start" shadow="md" borderRadius="md" p={2}>
+          {Object.values(items).map((itemProps) => (
+            <Item key={itemProps.route} {...itemProps} />
+          ))}
+        </Flex>
+      </Box>
+
+      {/* Small viewports */}
+      <Box as="nav" display={{ base: 'initial', lg: 'none' }}>
+        <SimpleLinkList items={getSimpleItems()} minWidth="full" selected={activeItem} label="Abstract Navigation" />
+      </Box>
+    </>
+  );
 };
