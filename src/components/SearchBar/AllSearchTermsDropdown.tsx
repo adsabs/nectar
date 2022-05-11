2d5e533aeac0b5a90ff7acb71d84f9053144c163
Jennifer Chen <shinyi.chen@gmail.com>
2022-05-11 12:37:47 -0400

    All search terms dropdown refactored

---
+++ src/components/SearchBar/AllSearchTermsDropdown.tsx
@@ -0,0 +1,228 @@
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { Box, Flex, IconButton, Input, ListItem, usePopper, Text, Code, UnorderedList } from '@chakra-ui/react';
+import { ReactElement, useMemo, useState, forwardRef } from 'react';
+import { allSearchTerms, ISearchTermOption } from './models';
+import { useCombobox } from 'downshift';
+
+type ITermItem = Partial<ISearchTermOption> & {
+  type: 'group' | 'item';
+  title: string;
+};
+
+export interface IAllSearchTermsDropdown {
+  onSelect: (value: string) => void;
+}
+
+export const AllSearchTermsDropdown = ({ onSelect }: IAllSearchTermsDropdown): ReactElement => {
+  const allTermsItems = useMemo(() => {
+    const tmp: ITermItem[] = [];
+    Object.entries(allSearchTerms).forEach(([group, terms]) => {
+      tmp.push({
+        type: 'group',
+        title: group,
+      });
+      Object.values(terms).forEach((term) => {
+        tmp.push({
+          type: 'item',
+          ...term,
+        });
+      });
+    });
+    return tmp;
+  }, [allSearchTerms]);
+
+  const [items, setItems] = useState(allTermsItems);
+
+  const [showTooltipFor, setShowTooltipFor] = useState<ITermItem>(null);
+
+  const { popperRef: dropdownPopperRef, referenceRef: dropdownReferenceRef } = usePopper({
+    placement: 'bottom-start',
+  });
+
+  const { popperRef: tooltipPopperRef, referenceRef: tooltipReferenceRef } = usePopper({
+    placement: 'right-start',
+    offset: [40, 20],
+  });
+
+  const {
+    isOpen,
+    openMenu,
+    closeMenu,
+    getMenuProps,
+    getToggleButtonProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+    setInputValue,
+    setHighlightedIndex,
+    selectItem,
+  } = useCombobox({
+    items: items,
+    onIsOpenChange: ({ isOpen }) => {
+      setHighlightedIndex(-1);
+      setInputValue('');
+      selectItem(null);
+      if (!isOpen) {
+        setShowTooltipFor(null);
+      }
+    },
+    onHighlightedIndexChange: ({ highlightedIndex }) => {
+      const item = items[highlightedIndex];
+      if (isOpen && item && item.type === 'item') {
+        setShowTooltipFor(item);
+      }
+      // keep tooltip shown when highlighted index is invalid (this means mouse moved away from menu)
+      // only hide tooltip when menu is closed
+    },
+    onSelectedItemChange: ({ selectedItem }) => {
+      if (selectedItem) {
+        onSelect(selectedItem.value); // callback
+        setInputValue('');
+      }
+    },
+    onInputValueChange: ({ inputValue }) => {
+      if (!inputValue || inputValue.length === 0) {
+        setItems(allTermsItems);
+        setHighlightedIndex(1);
+      } else {
+        const filtered = allTermsItems.filter((item) => {
+          return item.type !== 'group' && item.title.toLowerCase().startsWith(inputValue.toLowerCase());
+        });
+        setItems(filtered);
+        if (filtered.length > 0) {
+          setHighlightedIndex(0);
+        } else {
+          setShowTooltipFor(null);
+        }
+      }
+    },
+    itemToString: (item) => item?.title ?? '',
+    menuId: 'allSearchTermsMenu',
+    inputId: 'allSearchTermsInput',
+    getItemId: (index) => `allSearchTermsItem-${index}`,
+  });
+
+  const toggleIsOpen = () => {
+    isOpen ? closeMenu() : openMenu();
+  };
+
+  return (
+    <Box
+      aria-label="all search terms dropdown"
+      {...getComboboxProps({
+        ref: (el: HTMLDivElement) => {
+          tooltipReferenceRef(el);
+          return el;
+        },
+      })}
+      w="fit-content"
+    >
+      <Flex>
+        <Input
+          placeholder="all search terms"
+          {...getInputProps({
+            ref: (el: HTMLInputElement) => {
+              dropdownReferenceRef(el);
+              return el;
+            },
+            onKeyDown: (event) => {
+              if (event.key === 'Enter') {
+                if (items.length === 0 || !isOpen) {
+                  // Prevent Downshift's default 'Enter' behavior if invalid input or no input
+                  (
+                    event.nativeEvent as typeof event.nativeEvent & { preventDownshiftDefault: boolean }
+                  ).preventDownshiftDefault = true;
+                  event.preventDefault(); // this will prevent entering for search
+                  closeMenu();
+                }
+              }
+            },
+          })}
+          onClick={toggleIsOpen}
+        />
+        <IconButton icon={<ChevronDownIcon />} {...getToggleButtonProps()} borderLeftRadius={0} tabIndex={0} />
+      </Flex>
+      <UnorderedList
+        zIndex={10}
+        bgColor="white"
+        border={isOpen && items.length > 0 ? '1px' : 'none'}
+        borderRadius={5}
+        borderColor="gray.200"
+        boxShadow="lg"
+        maxHeight="400px"
+        w="200px"
+        overflow="scroll"
+        {...getMenuProps({
+          ref: (el: HTMLUListElement) => {
+            dropdownPopperRef(el);
+            return el;
+          },
+        })}
+      >
+        {isOpen &&
+          items.map((term, index) => (
+            <ListItem
+              key={`${term.type}-${term.title}`}
+              color={term.type === 'group' ? 'gray.300' : 'gray.700'}
+              fontWeight={term.type === 'group' ? 'bold' : 'normal'}
+              backgroundColor={highlightedIndex === index ? 'blue.100' : 'auto'}
+              {...getItemProps({
+                item: term,
+                index,
+                disabled: term.type === 'group',
+              })}
+              p={2}
+              pl={term.type === 'group' ? 2 : 4}
+            >
+              {term.title}
+            </ListItem>
+          ))}
+      </UnorderedList>
+      {showTooltipFor && <SearchTermTooltip term={showTooltipFor} ref={tooltipPopperRef} />}
+    </Box>
+  );
+};
+
+interface ISearchTermTooltipProps {
+  term: ITermItem;
+}
+const SearchTermTooltip = forwardRef<HTMLDivElement, ISearchTermTooltipProps>(({ term }, ref) => {
+  return (
+    <Box
+      as="div"
+      ref={ref}
+      bg="white"
+      borderColor="gray.200"
+      borderWidth={1}
+      borderRadius={5}
+      w={300}
+      zIndex={10}
+      m={5}
+      data-testid="allSearchTooltip"
+    >
+      <Text color="gray.900" fontWeight="bold" backgroundColor="gray.100" p={2} data-testid="allSearchTooltipTitle">
+        {term.title}
+      </Text>
+      <Text p={2} dangerouslySetInnerHTML={{ __html: term.description }} data-testid="allSearchTooltipDesc" />
+      <Text p={2} data-testid="allSearchTooltipSyntax">
+        Syntax:
+        {term.syntax.map((s) => (
+          <span key={s}>
+            <br />
+            <Code>{s}</Code>
+          </span>
+        ))}
+      </Text>
+      <Text p={2} data-testid="allSearchTooltipExample">
+        Example:
+        {term.example.map((e) => (
+          <span key={e}>
+            <br />
+            <Code>{e}</Code>
+          </span>
+        ))}
+      </Text>
+    </Box>
+  );
+});
--- src/components/SearchBar/QuickFields.tsx
+++ src/components/SearchBar/QuickFields.tsx
@@ -1,21 +1,8 @@
-import { ChevronDownIcon } from '@chakra-ui/icons';
-import {
-  Box,
-  Button,
-  Code,
-  Flex,
-  HStack,
-  Menu,
-  MenuButton,
-  MenuGroup,
-  MenuItem,
-  MenuList,
-  Text,
-  usePopper,
-} from '@chakra-ui/react';
-import { memo, useState } from 'react';
+import { Button, Flex, HStack, Text } from '@chakra-ui/react';
+import { memo } from 'react';
 import { MouseEvent, ReactElement } from 'react';
-import { allSearchTerms, ISearchTermOption, quickfields } from './models';
+import { AllSearchTermsDropdown } from './AllSearchTermsDropdown';
+import { quickfields } from './models';
 
 export interface IQuickFieldsProps {
   onSelect: (value: string) => void;
@@ -50,7 +37,7 @@ export const QuickFields = memo(
             </Button>
           ))}
         </HStack>
-        <AllSearchTerms onSelect={handleASTSelect} />
+        <AllSearchTermsDropdown onSelect={handleASTSelect} />
       </Flex>
     );
   },
@@ -58,95 +45,3 @@ export const QuickFields = memo(
     return prev.onSelect === next.onSelect;
   },
 );
-
-const AllSearchTerms = ({ onSelect }: { onSelect: (value: string) => void }): ReactElement => {
-  const [showTooltipFor, setShowTooltipFor] = useState<ISearchTermOption>(null);
-  const { popperRef, referenceRef } = usePopper({
-    placement: 'right-start',
-  });
-
-  const handleClose = () => {
-    // give it some time to handle user click inside tooltip (links)
-    // before closing the tooltips
-    setTimeout(() => setShowTooltipFor(null), 10);
-  };
-
-  const handleSelectAllSearchTerms = (e: MouseEvent<HTMLElement>) => {
-    setShowTooltipFor(null);
-    const target = e.currentTarget as HTMLElement;
-    const value = target.getAttribute('value');
-    onSelect(value);
-  };
-
-  const handleToolTip = (option: ISearchTermOption) => {
-    setShowTooltipFor(option);
-  };
-
-  return (
-    <>
-      <Menu onClose={handleClose} isLazy={false}>
-        <MenuButton as={Button} variant="outline" rightIcon={<ChevronDownIcon />} data-testid="allSearchTermsMenu">
-          All Search Terms
-        </MenuButton>
-        <MenuList h="400px" overflow="scroll" zIndex="10" ref={referenceRef} data-testid="allSearchTermsMenuItems">
-          {Object.entries(allSearchTerms).map(([group, options]) => (
-            <MenuGroup title={group} fontWeight="bold" fontSize="lg" key={group}>
-              {Object.values(options).map((option) => (
-                <MenuItem
-                  key={option.id}
-                  onClick={handleSelectAllSearchTerms}
-                  value={option.value}
-                  fontSize="md"
-                  onMouseEnter={() => handleToolTip(option)}
-                  data-testid="allSearchTermsItem"
-                >
-                  {option.title}
-                </MenuItem>
-              ))}
-            </MenuGroup>
-          ))}
-        </MenuList>
-      </Menu>
-      {showTooltipFor && (
-        <Box
-          ref={popperRef}
-          bg="white"
-          borderColor="gray.200"
-          borderWidth={1}
-          borderRadius={5}
-          w={300}
-          zIndex={10}
-          m={5}
-          data-testid="allSearchTooltip"
-        >
-          <Text color="gray.900" fontWeight="bold" backgroundColor="gray.100" p={2} data-testid="allSearchTooltipTitle">
-            {showTooltipFor.title}
-          </Text>
-          <Text
-            p={2}
-            dangerouslySetInnerHTML={{ __html: showTooltipFor.description }}
-            data-testid="allSearchTooltipDesc"
-          />
-          <Text p={2} data-testid="allSearchTooltipSyntax">
-            Syntax:
-            {showTooltipFor.syntax.map((s) => (
-              <span key={s}>
-                <br />
-                <Code>{s}</Code>
-              </span>
-            ))}
-          </Text>
-          <Text p={2} data-testid="allSearchTooltipExample">
-            Example:
-            {showTooltipFor.example.map((e) => (
-              <span key={e}>
-                <br />
-                <Code>{e}</Code>
-              </span>
-            ))}
-          </Text>
-        </Box>
-      )}
-    </>
-  );
-};
