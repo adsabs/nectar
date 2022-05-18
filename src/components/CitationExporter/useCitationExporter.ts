239eb933dcc49e8b8f151c503dff31b889b265e4
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-05-18 17:11:03 -0400

    Remove old export page, add cache check on mount

--- src/components/CitationExporter/useCitationExporter.ts
+++ src/components/CitationExporter/useCitationExporter.ts
@@ -1,6 +1,14 @@
-import { ExportApiFormatKey, SolrSort, useGetExportCitation } from '@api';
+import {
+  ExportApiFormatKey,
+  exportCitationKeys,
+  fetchExportCitation,
+  IExportApiParams,
+  SolrSort,
+  useGetExportCitation,
+} from '@api';
 import { useMachine } from '@xstate/react/fsm';
 import { useEffect, useMemo } from 'react';
+import { useQueryClient } from 'react-query';
 import { generateMachine, ICitationExporterState } from './CitationExporter.machine';
 
 export interface IUseCitationExporterProps {
@@ -13,22 +21,44 @@ export interface IUseCitationExporterProps {
 export const useCitationExporter = ({ records, format, singleMode, sort }: IUseCitationExporterProps) => {
   const machine = useMemo(() => generateMachine({ format, records, singleMode, sort }), []);
   const [state, dispatch] = useMachine(machine);
+  const queryClient = useQueryClient();
+
+  // on mount, check the cache to see if we any records for this querykey, if not, we should trigger an initial load
+  // should usually have an entry since the data will be available from SSR
+  useEffect(() => {
+    (async () => {
+      const queryKey = exportCitationKeys.primary(state.context.params);
+      const cached = queryClient.getQueryData<IExportApiParams>(queryKey);
+      if (!cached) {
+        // no cached value, prefetch and submit it here since we know the params
+        await queryClient.prefetchQuery({
+          queryKey,
+          queryFn: fetchExportCitation,
+          meta: { params: state.context.params },
+        });
+        dispatch('SUBMIT');
+      }
+    })();
+  }, []);
 
   // trigger updates to machine state if incoming props change
   useEffect(() => dispatch({ type: 'SET_SINGLEMODE', payload: singleMode }), [singleMode]);
 
+  // watch for format changes
   useEffect(() => {
     if (format !== state.context.params.format) {
       dispatch({ type: 'SET_FORMAT', payload: format });
     }
   }, [format]);
 
+  // if we're in singleMode and format is changed, trigger a submit
   useEffect(() => {
     if (singleMode) {
       dispatch('SUBMIT');
     }
   }, [state.context.params.format, singleMode]);
 
+  // watch for changes to records
   useEffect(() => {
     // naively compare only the first record, this should be enough to determine
     // there is a difference
@@ -37,12 +67,14 @@ export const useCitationExporter = ({ records, format, singleMode, sort }: IUseC
     }
   }, [records]);
 
+  // watch for changes to sort
   useEffect(() => {
     if (sort !== state.context.params.sort) {
       dispatch({ type: 'SET_SORT', payload: sort });
     }
   }, [sort]);
 
+  // main result fetcher, this will not run unless we're in the 'fetching' state
   const result = useGetExportCitation(state.context.params, {
     enabled: state.matches('fetching'),
     keepPreviousData: state.matches('idle'),
@@ -63,10 +95,10 @@ export const useCitationExporter = ({ records, format, singleMode, sort }: IUseC
 
   // safety hatch, in case for some reason we get stuck in fetching mode
   useEffect(() => {
-    if (state.matches('fetching')) {
-      setTimeout(() => dispatch({ type: 'DONE' }), 5000);
+    if (state.matches('fetching') && result.data) {
+      dispatch('DONE');
     }
-  }, [state.value]);
+  }, [state.value, result.data]);
 
   return { ...result, state, dispatch };
 };
