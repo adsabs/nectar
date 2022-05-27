155747b9bc645ce09ffb510c66ef16d870e7f48e
Tim Hostetler <thostetler@users.noreply.github.com>
2022-05-27 12:35:29 -0400

    linting fix

--- src/components/__tests__/SearchQueryLink.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import { Default as SearchQueryLink } from '../__stories__/SearchQueryLink.stories';
-
-describe('SearchQueryLink', () => {
-  it.skip('renders without crashing', () => {
-    render(<SearchQueryLink params={{ q: '' }} />);
-  });
-});
