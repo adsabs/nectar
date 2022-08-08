f3ba35f855150c26c09d45f6568e43d9e019297c
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-08-08 05:47:00 -0400

    Small path fix

--- src/components/ResultList/Item/AbstractPreview.tsx
+++ src/components/ResultList/Item/AbstractPreview.tsx
@@ -2,10 +2,10 @@ import { IDocsEntity, useGetAbstractPreview } from '@api';
 import { IconButton } from '@chakra-ui/button';
 import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
 import { Flex, Text, VStack } from '@chakra-ui/layout';
+import { useToast } from '@chakra-ui/react';
 import { Collapse } from '@chakra-ui/transition';
 import { MathJax } from 'better-react-mathjax';
 import { ReactElement, useState } from 'react';
-import { toast } from 'react-toastify';
 
 export interface IAbstractPreviewProps {
   bibcode: IDocsEntity['bibcode'];
@@ -21,6 +21,7 @@ const text = {
 
 export const AbstractPreview = ({ bibcode }: IAbstractPreviewProps): ReactElement => {
   const [show, setShow] = useState(false);
+  const toast = useToast();
   const { data, isFetching, isSuccess } = useGetAbstractPreview(
     { bibcode },
     {
@@ -28,7 +29,7 @@ export const AbstractPreview = ({ bibcode }: IAbstractPreviewProps): ReactElemen
       keepPreviousData: true,
       onError: () => {
         // show toast notification on error, and close drawer
-        toast(text.error, { type: 'error' });
+        toast({ status: 'error', description: text.error });
         setShow(false);
       },
     },
