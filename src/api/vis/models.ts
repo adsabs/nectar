c028754e1ba61d49a55dba6862f28a7e46c95ab7
Jennifer Chen <shinyi.chen@gmail.com>
2022-09-08 11:02:17 -0400

    Update author and paper network API params

--- src/api/vis/models.ts
+++ src/api/vis/models.ts
@@ -2,11 +2,13 @@ import { IADSApiSearchParams } from '@api/search';
 import { IADSApiVisParams } from './types';
 
 export const getAuthorNetworkParams = ({ q, sort, rows }: IADSApiSearchParams): IADSApiVisParams => {
-  const query = JSON.stringify({ q: [`${q}`], sort: [`${sort.join(',')}`], rows: [`${rows}`] });
+  // rows needs to be wrapped by array to work
+  const query = JSON.stringify({ q: q, sort: sort.join(','), rows: [rows] });
   return { query: [query] };
 };
 
 export const getPaperNetworkParams = ({ q, sort, rows }: IADSApiSearchParams): IADSApiVisParams => {
-  const query = JSON.stringify({ q: [`${q}`], sort: [`${sort.join(',')}`], rows: [`${rows}`] });
+  // rows needs to be wrapped by array to work
+  const query = JSON.stringify({ q: q, sort: sort.join(','), rows: [rows] });
   return { query: [query] };
 };
