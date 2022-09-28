21bcc842ebf43687e0da3ec0e3b832ec24dcb1a1
Jennifer Chen <shinyi.chen@gmail.com>
2022-09-28 15:07:50 -0400

    style

--- src/components/Expandable/Expandable.tsx
+++ src/components/Expandable/Expandable.tsx
@@ -16,7 +16,7 @@ export const Expandable = ({ title, description, ...stackProps }: IExpandablePro
 
   return (
     <Stack direction="column" my={2} gap={1} {...stackProps}>
-      <Flex as="button" direction="row" onClick={handleToggle} color="blue.500" cursor="pointer">
+      <Flex as="a" direction="row" onClick={handleToggle} cursor="pointer">
         {isOpen ? <ChevronDownIcon w={5} h={5} mr={1} /> : <ChevronRightIcon w={5} h={5} mr={1} />}
         <Text>{title}</Text>
       </Flex>
