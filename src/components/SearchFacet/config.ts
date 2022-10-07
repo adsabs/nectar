9ff574be0807c375e20aef2dfbc2cdfa8a10bb80
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-10-07 18:41:38 -0400

    Updates to make search facet work with store

--- src/components/SearchFacet/config.ts
+++ src/components/SearchFacet/config.ts
@@ -1,28 +1,90 @@
+import { FacetField } from '@api';
 import { ISearchFacetProps } from './SearchFacet';
+import { SearchFacetID } from './types';
 
 const defaultLogic: ISearchFacetProps['logic'] = {
   single: ['limit to', 'exclude'],
   multiple: ['and', 'or', 'exclude'],
 };
 
-export const facetConfig: Omit<ISearchFacetProps, 'onQueryUpdate'>[] = [
-  { label: 'Author', field: 'author_facet_hier', hasChildren: true, logic: defaultLogic, defaultIsOpen: true },
-  { label: 'Collections', field: 'database', logic: defaultLogic, defaultIsOpen: true },
-  {
+export const facetConfig: Record<SearchFacetID, Omit<ISearchFacetProps, 'onQueryUpdate'>> = {
+  author: {
+    label: 'Author',
+    field: 'author_facet_hier' as FacetField,
+    hasChildren: true,
+    logic: defaultLogic,
+    storeId: 'author',
+  },
+  collections: {
+    label: 'Collections',
+    field: 'database' as FacetField,
+    logic: defaultLogic,
+    storeId: 'collections',
+  },
+  refereed: {
     label: 'Refereed',
-    field: 'property',
+    field: 'property' as FacetField,
     facetQuery: 'property:refereed',
     logic: defaultLogic,
-    defaultIsOpen: true,
     filter: ['refereed', 'notrefereed'],
+    storeId: 'refereed',
   },
-  { label: 'Institutions', field: 'aff_facet_hier', hasChildren: true, logic: defaultLogic },
-  { label: 'Keywords', field: 'keyword_facet', logic: defaultLogic },
-  { label: 'Publications', field: 'bibstem_facet', logic: defaultLogic },
-  { label: 'Bibgroups', field: 'bibgroup_facet', logic: defaultLogic },
-  { label: 'SIMBAD Objects', field: 'simbad_object_facet_hier', hasChildren: true, logic: defaultLogic },
-  { label: 'NED Objects', field: 'ned_object_facet_hier', hasChildren: true, logic: defaultLogic },
-  { label: 'Data', field: 'data_facet', logic: defaultLogic },
-  { label: 'Vizier Tables', field: 'vizier_facet', logic: defaultLogic },
-  { label: 'Publication Type', field: 'doctype_facet_hier', hasChildren: true, logic: defaultLogic },
-];
+  institutions: {
+    label: 'Institutions',
+    field: 'aff_facet_hier' as FacetField,
+    hasChildren: true,
+    logic: defaultLogic,
+    storeId: 'institutions',
+  },
+  keywords: {
+    label: 'Keywords',
+    field: 'keyword_facet' as FacetField,
+    logic: defaultLogic,
+    storeId: 'keywords',
+  },
+  publications: {
+    label: 'Publications',
+    field: 'bibstem_facet' as FacetField,
+    logic: defaultLogic,
+    storeId: 'publications',
+  },
+  bibgroups: {
+    label: 'Bibgroups',
+    field: 'bibgroup_facet' as FacetField,
+    logic: defaultLogic,
+    storeId: 'bibgroups',
+  },
+  simbad: {
+    label: 'SIMBAD Objects',
+    field: 'simbad_object_facet_hier' as FacetField,
+    hasChildren: true,
+    logic: defaultLogic,
+    storeId: 'simbad',
+  },
+  ned: {
+    label: 'NED Objects',
+    field: 'ned_object_facet_hier' as FacetField,
+    hasChildren: true,
+    logic: defaultLogic,
+    storeId: 'ned',
+  },
+  data: {
+    label: 'Data',
+    field: 'data_facet' as FacetField,
+    logic: defaultLogic,
+    storeId: 'data',
+  },
+  vizier: {
+    label: 'Vizier Tables',
+    field: 'vizier_facet' as FacetField,
+    logic: defaultLogic,
+    storeId: 'vizier',
+  },
+  pubtype: {
+    label: 'Publication Type',
+    field: 'doctype_facet_hier' as FacetField,
+    hasChildren: true,
+    logic: defaultLogic,
+    storeId: 'pubtype',
+  },
+};
