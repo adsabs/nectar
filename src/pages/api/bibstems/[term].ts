780a4cc1c4687579633764458e0e5c1dae992816
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-10-13 02:16:52 -0400

    update bibstem api algo

--- src/pages/api/bibstems/[term].ts
+++ src/pages/api/bibstems/[term].ts
@@ -4,13 +4,28 @@ import DOMPurify from 'isomorphic-dompurify';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import terms from 'public/data/bibstems/bibstems.json';
 import index from 'public/data/bibstems/index.json';
+import topTerms from 'public/data/bibstems/topterms.json';
 import { map, prop } from 'ramda';
 
-export interface BibstemSearchRequest extends NextApiRequest {
+export interface SearchRequest extends NextApiRequest {
   query: {
     term: string;
   };
 }
+
+/**
+ * Ranking function for sorting results
+ *
+ * So that a bibstem towards the bottom of the list
+ * will receive a lower overall ranking
+ */
+const getRank = (value: string, score: number) => score * Math.log(2 + getPosition(value));
+
+// retrieve the index position (if existing) in the `Top Terms` list
+const getPosition = (value: string) => {
+  const index = topTerms.indexOf(value);
+  return index === -1 ? topTerms.length : index;
+};
 
 // create a fuse instance using pre-generated index
 const fuse = new Fuse<IBibstemOption>(
@@ -21,13 +36,21 @@ const fuse = new Fuse<IBibstemOption>(
       { name: 'label', weight: 0.3 },
     ],
     isCaseSensitive: false,
+    ignoreLocation: true,
+    includeScore: true,
+    sortFn: (a, b) => {
+      const valueA = (a.item as { '0'?: { v: string } })[0]?.v;
+      const valueB = (b.item as { '0'?: { v: string } })[0]?.v;
+      return getRank(valueA, a.score) - getRank(valueB, b.score);
+    },
   },
   Fuse.parseIndex(index),
 );
 
-const formatResult = map(prop('item'));
+// extract `item` from the result list
+const formatResult = (result: Fuse.FuseResult<IBibstemOption>[]) => map(prop('item'), result);
 
-export default (req: BibstemSearchRequest, res: NextApiResponse<IBibstemOption[] | { error: string }>) => {
+export default (req: SearchRequest, res: NextApiResponse<IBibstemOption[] | { error: string }>) => {
   try {
     const value = DOMPurify.sanitize(req.query.term);
 
