26a3f847c558b35d0ffc0dbfc174c3ea18ea2823
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-10-31 03:48:23 -0400

    Updates to second order component

--- src/components/ResultList/SecondOrderOpsLinks.tsx
+++ src/components/ResultList/SecondOrderOpsLinks.tsx
@@ -25,13 +25,6 @@ const selectors = {
   docs: (state: AppState) => state.docs.selected,
 };
 
-// based on the type of docs query, create a new FQ with that value
-const getQuery = (type: 'limit' | 'exclude', qid: string, query: Q.Query) => {
-  const label = `selection_${type}`;
-  const q = type === 'limit' ? `docs(${qid})` : `-docs(${qid})`;
-  return Q.setFQ(label, q, query, { asIs: true });
-};
-
 interface State {
   enabled: boolean;
   type: 'limit' | 'exclude';
@@ -53,16 +46,23 @@ const useSecondOrderOps = () => {
   const selected = useStore(selectors.docs);
   const router = useRouter();
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { data: res } = useVaultBigQuerySearch(selected, { enabled: state.enabled });
+  const { data: qResult } = useVaultBigQuerySearch(selected, { enabled: state.enabled });
 
   useEffect(() => {
-    if (res?.qid && state.type !== null) {
+    if (state.type !== null && qResult?.qid) {
       // get qid from response
-      const qid = res.qid;
+      const selectionQID = qResult.qid;
 
       // parse and create new search params
       const parsedQuery = parseQueryFromUrl(router.asPath);
-      const search = makeSearchParams(getQuery(state.type, qid, parsedQuery) as IADSApiSearchParams);
+
+      const params = Q.setFQ(
+        'selection',
+        state.type === 'exclude' ? `*:* NOT docs(${selectionQID})` : `docs(${selectionQID})`,
+        parsedQuery,
+      );
+
+      const search = makeSearchParams(params as IADSApiSearchParams);
 
       // reset state, so enabled doesn't persist
       dispatch({ type: 'reset' });
@@ -70,7 +70,7 @@ const useSecondOrderOps = () => {
       // trigger the new search
       void router.push({ pathname: '/search', search });
     }
-  }, [res, state.type]);
+  }, [qResult, state.type]);
 
   return {
     onLimit: () => dispatch({ type: 'setType', payload: 'limit' }),
