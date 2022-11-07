34d98026370c13f88627b4743b96ac310d068636
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-11-07 16:35:56 -0500

    Various updates to configs, removes some tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
@@ -13,4 +14,10 @@ export default defineConfig({
       reporter: 'lcov',
     },
   },
+  resolve: {
+    alias: {
+      "react/jsx-dev-runtime.js": resolve(__dirname, "node_modules/react/jsx-dev-runtime.js"),
+      "react/jsx-runtime.js": resolve(__dirname, "node_modules/react/jsx-runtime.js")
+    }
+  }
 });
