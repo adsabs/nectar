c4cad534424ed7d638748c3025d40bc6acbe9214
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2022-12-31 12:17:48 -0500

    update download file hook to use file-saver

--- src/hooks/useDownloadFile.ts
+++ src/hooks/useDownloadFile.ts
@@ -1,11 +1,12 @@
 import { noop } from '@utils';
 import { formatISO } from 'date-fns';
+import { saveAs } from 'file-saver';
 import { parse } from 'path';
 import { isFunction } from 'ramda-adjunct';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useIsClient } from './useIsClient';
 
-interface IuseDownloadFileOptions {
+export interface IUseDownloadFileOptions {
   filename?: string | (() => string);
   type?: FileType;
   timeout?: number;
@@ -13,31 +14,38 @@ interface IuseDownloadFileOptions {
   onDownloaded?: () => void;
 }
 
-export const useDownloadFile = (value: string | (() => string), options?: IuseDownloadFileOptions) => {
+export const useDownloadFile = (value: string | (() => string), options?: IUseDownloadFileOptions) => {
   const { timeout = 300, filename = 'download.txt', appendDate = false, type = 'TEXT', onDownloaded = noop } = options;
   const isClient = useIsClient();
   const [hasDownloaded, setHasDownloaded] = useState(false);
   const [isDownloading, setIsDownloading] = useState(false);
-  const [href, setHref] = useState('');
   const meta = getMetaData(type);
   const file = getFileName(filename, appendDate, meta);
 
-  useEffect(() => {
-    if (value && type) {
-      setHref(createUrl(value, getMetaData(type)[0]));
+  // generate URI for the passed in value
+  const href = useMemo(() => {
+    if (typeof window !== 'undefined') {
+      const blob = new window.Blob(typeof value === 'function' ? [value()] : [value], { type });
+      return window.URL.createObjectURL(blob);
     }
   }, [value, type]);
 
   const onDownload = useCallback(() => {
     setIsDownloading(true);
 
-    // generate a link and add it to the dom
-    const link = document.createElement('a');
-    link.setAttribute('href', href);
-    link.setAttribute('download', file);
+    if (type === 'BROWSER' && isClient) {
+      // generate a link and add it to the dom
+      const link = document.createElement('a');
+      link.setAttribute('href', href);
+      link.setAttribute('download', file);
 
-    // if browser type, open new tab, otherwise click link to start download
-    type === 'BROWSER' && isClient ? window.open(link.href, '_blank') : link.click();
+      // open the link in a new tab
+      window.open(link.href, '_blank');
+      link.remove();
+    } else {
+      // save the file
+      saveAs(href, file, { autoBom: false });
+    }
 
     // let subscribers know we finished
     setHasDownloaded(true);
@@ -45,19 +53,12 @@ export const useDownloadFile = (value: string | (() => string), options?: IuseDo
     // call finished callback
     onDownloaded();
 
-    link.remove();
     setTimeout(() => setHasDownloaded(false), timeout);
   }, [value, file]);
 
   useEffect(() => setIsDownloading(false), [hasDownloaded]);
 
   return { onDownload, hasDownloaded, isDownloading, value, filename: file, linkHref: href };
-};
-
-const createUrl = (value: string | (() => string), type: string) => {
-  const content = typeof value === 'function' ? value() : value;
-  const blob = new Blob([content], { type });
-  return window.URL.createObjectURL(blob);
 };
 
 const getDateString = () => formatISO(new Date());
