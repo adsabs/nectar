56e006ec767dbcc26c96d1bf5b230b2ac1e8c775
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-03-25 12:55:41 -0400

    Remove old server directory

--- server/middlewares/logger.ts
+++
@@ -1,9 +0,0 @@
-import expressPinoLogger from 'express-pino-logger';
-
-const config: expressPinoLogger.Options = {
-  name: 'nectar',
-  level: process.env.LOG_LEVEL || 'silent',
-  prettyPrint: true,
-};
-
-export const logger = expressPinoLogger(config);
