1a2c77f99822993f4e00354d68ed06301d837f84
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-05-10 01:11:31 -0400

    Add pages, login in navbar

---
+++ src/pages/user/orcid/OAuth.tsx
@@ -0,0 +1,45 @@
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { Alert, Container, Spinner } from '@chakra-ui/react';
+import { isString } from '@utils';
+import { useOrcidExchangeToken } from '@api/orcid/orcid';
+import { isArray } from 'ramda-adjunct';
+import { Box } from '@chakra-ui/layout';
+import { useOrcid } from '@hooks/useOrcid';
+import { useEffect } from 'react';
+import { AppState, useStore } from '@store';
+
+const setOrcidUserSelector = (state: AppState) => state.setOrcidUser;
+
+const OrcidPage: NextPage = () => {
+  const router = useRouter();
+  const { toggleOrcidMode } = useOrcid();
+  const setOrcidUser = useStore(setOrcidUserSelector);
+  const code = isArray(router.query.code) ? router.query.code[0] : router.query.code;
+
+  const { data } = useOrcidExchangeToken(
+    { code },
+    {
+      enabled: isString(code),
+    },
+  );
+
+  useEffect(() => {
+    if (data) {
+      setOrcidUser(data);
+      toggleOrcidMode();
+      void router.replace('/user/orcid');
+    }
+  }, [data]);
+
+  return (
+    <Container centerContent my="8">
+      <Alert status="info" display="flex" justifyContent="center">
+        <Spinner />
+        <Box ml="2">Authenticating with ORCiD</Box>
+      </Alert>
+    </Container>
+  );
+};
+
+export default OrcidPage;
---
+++ src/pages/user/orcid/index.tsx
@@ -0,0 +1,29 @@
+import { NextPage } from 'next';
+import { Heading } from '@chakra-ui/layout';
+import { useOrcidGetProfile } from '@api/orcid/orcid';
+import { useOrcid } from '@hooks/useOrcid';
+import { Code, List, ListItem } from '@chakra-ui/react';
+
+const OrcidPage: NextPage = () => {
+  const { user } = useOrcid();
+  const { data } = useOrcidGetProfile({ orcid: user?.orcid }, { enabled: typeof user?.orcid === 'string' });
+  return (
+    <>
+      <Heading as="h2">ORCiD</Heading>
+      <Heading size="md" as="h3">
+        Works
+      </Heading>
+      {data ? (
+        <List>
+          {Object.values(data).map((work) => (
+            <ListItem>
+              <Code>{JSON.stringify(work, null, 2)}</Code>
+            </ListItem>
+          ))}
+        </List>
+      ) : null}
+    </>
+  );
+};
+
+export default OrcidPage;
