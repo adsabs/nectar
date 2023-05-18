1441583d96c62c6b1bcc07921dd251d8444ff278
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-05-18 17:10:04 -0400

    add profile to hook

--- src/pages/user/orcid/index.tsx
+++ src/pages/user/orcid/index.tsx
@@ -1,28 +1,20 @@
 import { NextPage } from 'next';
 import { Heading } from '@chakra-ui/layout';
-import { useOrcidGetProfile } from '@api/orcid/orcid';
 import { useOrcid } from '@hooks/orcid/useOrcid';
 import { Code, List, ListItem } from '@chakra-ui/react';
-import { isValidIOrcidUser } from '@api/orcid/models';
 
 const OrcidPage: NextPage = () => {
-  const { user } = useOrcid();
-  const { data } = useOrcidGetProfile(
-    { user },
-    {
-      enabled: isValidIOrcidUser(user),
-    },
-  );
+  const { profile } = useOrcid();
   return (
     <>
       <Heading as="h2">ORCiD</Heading>
       <Heading size="md" as="h3">
         Works
       </Heading>
-      {data ? (
+      {profile ? (
         <List>
-          {Object.values(data).map((work) => (
-            <ListItem>
+          {Object.values(profile).map((work) => (
+            <ListItem key={work.identifier}>
               <Code>{JSON.stringify(work, null, 2)}</Code>
             </ListItem>
           ))}
