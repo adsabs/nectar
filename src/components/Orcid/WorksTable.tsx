2d6d7f453763555efdc5102a3eac0932c4896fbe
Jennifer Chen <shinyi.chen@gmail.com>
2023-05-25 11:28:41 -0700

    Orcid actions - partial

--- src/components/Orcid/WorksTable.tsx
+++ src/components/Orcid/WorksTable.tsx
@@ -1,11 +1,14 @@
 import { getSearchParams, useSearch } from '@api';
 import { IOrcidProfile } from '@api/orcid/types';
 import { IOrcidProfileEntry } from '@api/orcid/types/orcid-profile';
-import { Stack, Heading, Table, Thead, Tr, Th, Tbody, Td, Text, Box } from '@chakra-ui/react';
+import { Stack, Heading, Table, Thead, Tr, Th, Tbody, Td, Text, Box, useToast } from '@chakra-ui/react';
 import { Select, SelectOption } from '@components/Select';
 import { SimpleLink } from '@components/SimpleLink';
 import { useOrcid } from '@lib/orcid/useOrcid';
-import { useMemo, useState } from 'react';
+import { useRemoveWorks } from '@lib/orcid/useRemoveWorks';
+import { useEffect, useMemo, useState } from 'react';
+import { Actions } from './Actions';
+import { isInSciX } from './Utils';
 
 // TODO: pagination
 
@@ -28,6 +31,10 @@ const compareFn = (sortByField: 'title' | 'updated' | 'status', direction: 'asc'
 };
 
 export const WorksTable = () => {
+  const toast = useToast({
+    duration: 2000,
+  });
+
   const { user, profile } = useOrcid();
 
   const allWorks: IOrcidProfile = profile;
@@ -68,17 +75,59 @@ export const WorksTable = () => {
     const allWorsList = allWorks ? Object.values(allWorks) : [];
     switch (selectedFilter.id) {
       case 'all':
-        return allWorsList.sort(compareFn(sortBy, 'asc')) as IOrcidProfileEntry[];
+        return allWorsList.sort(compareFn(sortBy, 'asc'));
       case 'orcid':
-        return allWorsList.filter((w) => w.status !== null).sort(compareFn(sortBy, 'asc')) as IOrcidProfileEntry[];
+        return allWorsList.filter((w) => w.status !== null).sort(compareFn(sortBy, 'asc'));
       case 'not-orcid':
-        return allWorsList.filter((w) => w.status === null).sort(compareFn(sortBy, 'asc')) as IOrcidProfileEntry[];
+        return allWorsList.filter((w) => w.status === null).sort(compareFn(sortBy, 'asc'));
       case 'not-scix':
         return allWorsList
           .filter((w) => w.source.indexOf('NASA Astrophysics Data System') === -1)
-          .sort(compareFn(sortBy, 'asc')) as IOrcidProfileEntry[];
+          .sort(compareFn(sortBy, 'asc'));
     }
   }, [allWorks, selectedFilter, sortBy]);
+
+  // TODO: enable add claim
+  // TODO: enable sync
+  // enable delete claim
+  const { removeWorks, isSuccess: removeWorksSuccessful, error: removeWorksError } = useRemoveWorks();
+
+  // TODO: adding claim from scix
+  // TODO: sync to orcid
+  // removing claim from scix
+  useEffect(() => {
+    if (removeWorksSuccessful) {
+      toast({
+        status: 'success',
+        title: 'Successfully submitted claim request',
+      });
+    }
+    if (removeWorksError) {
+      toast({
+        status: 'error',
+        title: removeWorksError.message,
+      });
+    }
+  }, [removeWorksSuccessful, removeWorksError]);
+
+  const handleFilterOptionsSelected = (option: SelectOption) => {
+    setSelectedFilter(option);
+  };
+
+  // TODO: add claim handler
+  const handleAddClaim = () => {
+    return;
+  };
+
+  // TODO: sync to orcid handler
+  const handleSyncToOrcid = () => {
+    return;
+  };
+
+  // Delete claim handler
+  const handleDeleteClaim = (putcode: IOrcidProfileEntry['putcode']) => {
+    removeWorks([typeof putcode === 'number' ? putcode.toString() : putcode]);
+  };
 
   return (
     <Stack flexGrow={{ base: 0, lg: 6 }}>
@@ -90,21 +139,21 @@ export const WorksTable = () => {
       </SimpleLink>
       <Text>Claims take up to 24 hours to be indexed in SciX</Text>
       {!user && !profile && <>Loading...</>}
+      <Box w="350px">
+        <Select
+          options={filterOptions}
+          value={selectedFilter}
+          label="Filter"
+          id="orcid-filter-options"
+          stylesTheme="default"
+          onChange={handleFilterOptionsSelected}
+        />
+      </Box>
       {user && profile ? (
         displayedWorks.length === 0 ? (
           <Text>No papers found</Text>
         ) : (
           <>
-            <Box w="350px">
-              <Select
-                options={filterOptions}
-                value={selectedFilter}
-                label="Filter"
-                id="orcid-filter-options"
-                stylesTheme="default"
-                onChange={handleFilterOptionsSelected}
-              />
-            </Box>
             <Table>
               <Thead>
                 <Tr>
@@ -132,7 +181,14 @@ export const WorksTable = () => {
                     <Td>{work.source.join(',')}</Td>
                     <Td>{new Date(work.updated).toLocaleDateString('en-US')}</Td>
                     <Td>{work.status ?? 'unclaimed'}</Td>
-                    <Td></Td>
+                    <Td>
+                      <Actions
+                        profile={work}
+                        onAddClaim={handleAddClaim}
+                        onDeleteClaim={handleDeleteClaim}
+                        onSyncToOrcid={handleSyncToOrcid}
+                      />
+                    </Td>
                   </Tr>
                 ))}
               </Tbody>
--- src/components/Orcid/index.ts
+++ src/components/Orcid/index.ts
@@ -1,2 +1,3 @@
+export * from './Actions';
 export * from './UserSettings';
 export * from './WorksTable';
