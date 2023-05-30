b6cb3b1c8c1d8dbad7c9778270bc8e1d1082cdc7
Jennifer Chen <shinyi.chen@gmail.com>
2023-05-30 14:25:55 -0700

    Simple orcid menu

---
+++ src/components/Orcid/SimpleAction.tsx
@@ -0,0 +1,35 @@
+import { Button, HStack, IconButton, Menu, MenuButton, MenuItem, MenuList, Text, Tooltip } from '@chakra-ui/react';
+import { OrcidInactiveLogo, OrcidLogo } from '@components/images';
+
+export interface ISimpleActionProps {
+  isClaimed: boolean;
+  onAddClaim: () => void;
+  onDeleteClaim: () => void;
+}
+
+// if status is null,
+export const SimpleAction = ({ isClaimed, onAddClaim, onDeleteClaim }: ISimpleActionProps) => {
+  return (
+    <>
+      {isClaimed ? (
+        <Tooltip label="Claim from SciX">
+          <Button variant="outline" size="xs" color="gray.500" onClick={onAddClaim} mr={1}>
+            <HStack spacing={1}>
+              <OrcidInactiveLogo className="flex-shrink-0 w-4 h-4" aria-hidden />
+              <Text fontSize="xs">Claim</Text>
+            </HStack>
+          </Button>
+        </Tooltip>
+      ) : (
+        <Tooltip label="Delete claim from SciX">
+          <Button variant="outline" size="xs" color="gray.500" onClick={onDeleteClaim} mr={1}>
+            <HStack spacing={1}>
+              <OrcidLogo className="flex-shrink-0 w-4 h-4" aria-hidden />
+              <Text fontSize="xs">Delete Claim</Text>
+            </HStack>
+          </Button>
+        </Tooltip>
+      )}
+    </>
+  );
+};
