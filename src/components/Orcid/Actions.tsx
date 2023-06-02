b62c172d64bdd5e0956143d0ffc190f84db7d802
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-06-02 17:32:50 -0400

    Update to WorksTable and Actions
    * Moves all the hook calls to the direct component calling, no callbacks
    
    * Removes all top-level error/success handling to the component level
    
    * Uses the callbacks provided on the mutate function

--- src/components/Orcid/Actions.tsx
+++ src/components/Orcid/Actions.tsx
@@ -1,63 +1,144 @@
 import { IOrcidProfileEntry } from '@api/orcid/types/orcid-profile';
 import { ChevronDownIcon } from '@chakra-ui/icons';
-import { Button, HStack, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import {
+  Button,
+  ButtonProps,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useToast,
+  UseToastOptions,
+} from '@chakra-ui/react';
 import { OrcidInactiveLogo, OrcidLogo } from '@components';
 import { isClaimedBySciX, isInSciX } from './Utils';
+import { MenuItemProps } from '@chakra-ui/menu';
+import { useUpdateWork } from '@lib/orcid/useUpdateWork';
+import { useAddWorks } from '@lib/orcid/useAddWorks';
+import { useRemoveWorks } from '@lib/orcid/useRemoveWorks';
 
 export interface IActionProps {
-  profile: IOrcidProfileEntry;
-  onAddClaim: (identifier: string) => void;
-  onDeleteClaim: (identifier: string) => void;
-  onSyncToOrcid: (identifier: string) => void;
+  work: IOrcidProfileEntry;
 }
 
-export const Actions = ({ profile, onAddClaim, onDeleteClaim, onSyncToOrcid }: IActionProps) => {
-  const handleAddClaim = () => {
-    onAddClaim(profile.identifier);
-  };
+const TOAST_DEFAULTS: UseToastOptions = {
+  duration: 2000,
+};
 
-  const handleDeleteClaim = () => {
-    onDeleteClaim(profile.identifier);
-  };
-
-  const handleSyncToOrcid = () => {
-    onSyncToOrcid(profile.identifier);
-  };
-
-  const claimedBySciX = isClaimedBySciX(profile);
-
-  const inSciX = isInSciX(profile);
+export const Actions = ({ work }: IActionProps) => {
+  const claimedBySciX = isClaimedBySciX(work);
+  const inSciX = isInSciX(work);
 
   return (
     <>
-      {profile.status ? (
+      {work.status ? (
         <Menu>
-          <MenuButton as={Button} variant="outline" rightIcon={<ChevronDownIcon />} color="gray.500" w={28}>
+          <MenuButton
+            as={Button}
+            isDisabled={!inSciX || !claimedBySciX}
+            variant="outline"
+            rightIcon={<ChevronDownIcon />}
+            color="gray.500"
+            w={28}
+          >
             <HStack spacing={1}>
               <OrcidLogo className="flex-shrink-0 w-4 h-4" aria-hidden />
               <span>Actions</span>
             </HStack>
           </MenuButton>
           <MenuList>
-            <MenuItem isDisabled={!inSciX || !claimedBySciX} onClick={handleSyncToOrcid}>
-              Sync to ORCiD
-            </MenuItem>
-            <MenuItem isDisabled={!inSciX || claimedBySciX} onClick={handleAddClaim}>
-              Claim from SciX
-            </MenuItem>
-            <MenuItem isDisabled={!inSciX || !claimedBySciX} onClick={handleDeleteClaim}>
-              Delete claim from SciX
-            </MenuItem>
+            <SyncToOrcidMenuItem identifier={work.identifier} isDisabled />
+            <AddClaimMenuItem identifier={work.identifier} />
+            <DeleteClaimMenuItem identifier={work.identifier} />
           </MenuList>
         </Menu>
       ) : (
-        <Button variant="outline" color="gray.500" onClick={handleAddClaim} w={28}>
-          <HStack spacing={1}>
-            <OrcidInactiveLogo className="flex-shrink-0 w-4 h-4" aria-hidden />
-            <span>Claim</span>
-          </HStack>
-        </Button>
+        <AddToOrcidButton identifier={work.identifier} />
       )}
     </>
+  );
+};
+
+interface IOrcidActionProps extends MenuItemProps {
+  identifier: string;
+}
+
+const AddToOrcidButton = (props: ButtonProps & { identifier: string }) => {
+  const { identifier, ...buttonProps } = props;
+  const toast = useToast(TOAST_DEFAULTS);
+  const { addWorks } = useAddWorks(
+    {},
+    {
+      onSuccess: () => {
+        toast({ status: 'success', title: 'Successfully submitted sync request' });
+      },
+      onError: (error) => {
+        toast({ status: 'error', title: 'Unable to submit request', description: error });
+      },
+    },
+  );
+
+  return (
+    <Button variant="outline" color="gray.500" onClick={() => addWorks([identifier])} w={28} {...buttonProps}>
+      <HStack spacing={1}>
+        <OrcidInactiveLogo className="flex-shrink-0 w-4 h-4" aria-hidden />
+        <span>Claim</span>
+      </HStack>
+    </Button>
+  );
+};
+
+const SyncToOrcidMenuItem = (props: IOrcidActionProps) => {
+  const { identifier, ...menuItemProps } = props;
+  const { updateWork } = useUpdateWork();
+
+  return (
+    <MenuItem onClick={() => updateWork(identifier)} {...menuItemProps}>
+      Sync to ORCiD
+    </MenuItem>
+  );
+};
+
+const AddClaimMenuItem = (props: IOrcidActionProps) => {
+  const { identifier, ...menuItemProps } = props;
+  const toast = useToast(TOAST_DEFAULTS);
+  const { addWorks } = useAddWorks(
+    {},
+    {
+      onSuccess: () => {
+        toast({ status: 'success', title: 'Successfully submitted sync request' });
+      },
+      onError: (error) => {
+        toast({ status: 'error', title: 'Unable to submit request', description: error });
+      },
+    },
+  );
+
+  return (
+    <MenuItem onClick={() => addWorks([identifier])} {...menuItemProps}>
+      Claim from SciX
+    </MenuItem>
+  );
+};
+const DeleteClaimMenuItem = (props: IOrcidActionProps) => {
+  const { identifier, ...menuItemProps } = props;
+  const toast = useToast(TOAST_DEFAULTS);
+  const { removeWorks } = useRemoveWorks(
+    {},
+    {
+      onSuccess: () => {
+        toast({ status: 'success', title: 'Successfully submitted remove claim request' });
+      },
+      onError: (error) => {
+        toast({ status: 'error', title: 'Unable to submit request', description: error });
+      },
+    },
+  );
+
+  return (
+    <MenuItem onClick={() => removeWorks([identifier])} {...menuItemProps}>
+      Delete claim from SciX
+    </MenuItem>
   );
 };
