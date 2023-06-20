312e7770b23fb1817f8e86ed93a8a676a4f63bda
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-06-19 22:04:57 -0400

    improvements to loading/error states for profile

---
+++ src/lib/orcid/useOrcidProfile.ts
@@ -0,0 +1,24 @@
+import { AppState, useStore } from '@store';
+import { useOrcidGetProfile } from '@api/orcid';
+import { isValidIOrcidUser } from '@api/orcid/models';
+
+const isAuthenticatedSelector = (state: AppState) => state.orcid.isAuthenticated;
+const orcidUserSelector = (state: AppState) => state.orcid.user;
+
+export const useOrcidProfile = (options?: Parameters<typeof useOrcidGetProfile>[1]) => {
+  const isAuthenticated = useStore(isAuthenticatedSelector);
+  const user = useStore(orcidUserSelector);
+
+  const { data: profile, ...result } = useOrcidGetProfile(
+    { user, full: true, update: true },
+    {
+      enabled: !!options.enabled || (isAuthenticated && isValidIOrcidUser(user)),
+      ...options,
+    },
+  );
+
+  return {
+    profile,
+    ...result,
+  };
+};
