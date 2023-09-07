20e9af3c5e3b2a5385b0f54d847bd467f0c4cd80
Jennifer Chen <shinyi.chen@gmail.com>
2023-09-07 13:42:50 -0700

    Biblib API

---
+++ src/api/biblib/libraries.ts
@@ -0,0 +1,352 @@
+import api, {
+  ADSMutation,
+  ADSQuery,
+  ApiRequestConfig,
+  ApiTargets,
+  IADSApiLibraryAddParams,
+  IADSApiLibraryAddResponse,
+  IADSApiLibraryDeleteParams,
+  IADSApiLibraryDeleteResponse,
+  IADSApiLibraryDocumentParams,
+  IADSApiLibraryDocumentResponse,
+  IADSApiLibraryEditMetaParams,
+  IADSApiLibraryEditMetaResponse,
+  IADSApiLibraryEntityParams,
+  IADSApiLibraryEntityResponse,
+  IADSApiLibraryOperationParams,
+  IADSApiLibraryOperationResponse,
+  IADSApiLibraryPermissionParams,
+  IADSApiLibraryPermissionResponse,
+  IADSApiLibraryPermissionUpdateParams,
+  IADSApiLibraryPermissionUpdateResponse,
+  IADSApiLibraryQueryParams,
+  IADSApiLibraryQueryResponse,
+  IADSApiLibraryQueryUpdateParams,
+  IADSApiLibraryQueryUpdateResponse,
+  IADSApiLibraryResponse,
+  IADSApiLibraryTransferParams,
+  IADSApiLibraryTransferResponse,
+} from '@api';
+import { MutationFunction, QueryFunction, useMutation, useQuery } from '@tanstack/react-query';
+import { omit } from 'ramda';
+
+export enum LIBRARY_API_KEYS {
+  LIBRARIES = 'library/libraries',
+  LIBRARY = 'library/library',
+  ADD = 'library/add',
+  DELETE = 'library/delete',
+  EDIT = 'library/edit',
+  OPERATION = 'library/operation',
+  DOCUMENT = 'library/document',
+  QUERY = 'library/query',
+  QUERY_UPDATE = 'library/query-update',
+  PERMISSION = 'library/permission',
+  PERMISSION_UPDATE = 'library/permission-update',
+  TRANSFER = 'library/transfer',
+}
+
+export const librariesKeys = {
+  libraries: () => [LIBRARY_API_KEYS.LIBRARIES] as const,
+  library: (params: IADSApiLibraryEntityParams) => [LIBRARY_API_KEYS.LIBRARY, params],
+  add: () => [LIBRARY_API_KEYS.ADD] as const,
+  delete: () => [LIBRARY_API_KEYS.DELETE] as const,
+  edit: () => [LIBRARY_API_KEYS.EDIT] as const,
+  operation: () => [LIBRARY_API_KEYS.OPERATION] as const,
+  document: () => [LIBRARY_API_KEYS.DOCUMENT] as const,
+  query: () => [LIBRARY_API_KEYS.QUERY] as const,
+  query_update: () => [LIBRARY_API_KEYS.QUERY_UPDATE] as const,
+  permission: (params: IADSApiLibraryPermissionParams) => [LIBRARY_API_KEYS.PERMISSION, params],
+  permission_update: () => [LIBRARY_API_KEYS.PERMISSION_UPDATE] as const,
+  transfer: () => [LIBRARY_API_KEYS.TRANSFER] as const,
+};
+
+// libraries
+
+export const useGetLibraries: ADSQuery<unknown, IADSApiLibraryResponse> = () => {
+  return useQuery({
+    queryKey: librariesKeys.libraries(),
+    queryFn: fetchLibraries,
+  });
+};
+
+export const fetchLibraries: QueryFunction<IADSApiLibraryResponse> = async () => {
+  const config: ApiRequestConfig = {
+    method: 'GET',
+    url: ApiTargets.LIBRARIES,
+  };
+
+  const { data } = await api.request<IADSApiLibraryResponse>(config);
+  return data;
+};
+
+export const useAddLibrary: ADSMutation<IADSApiLibraryAddResponse, undefined, IADSApiLibraryAddParams> = (
+  _,
+  options,
+) => {
+  return useMutation({
+    mutationKey: librariesKeys.add(),
+    mutationFn: addLibrary,
+    ...options,
+  });
+};
+
+export const addLibrary: MutationFunction<IADSApiLibraryAddResponse, IADSApiLibraryAddParams> = async (params) => {
+  const config: ApiRequestConfig = {
+    method: 'POST',
+    url: `${ApiTargets.LIBRARIES}`,
+    data: params,
+  };
+
+  const { data } = await api.request<IADSApiLibraryAddResponse>(config);
+  return data;
+};
+
+export const useGetLibraryEntity: ADSQuery<IADSApiLibraryEntityParams, IADSApiLibraryEntityResponse> = (
+  params,
+  options,
+) => {
+  return useQuery({
+    queryKey: librariesKeys.library(params),
+    queryFn: fetchLibraryEntity,
+    meta: { params },
+    ...options,
+  });
+};
+
+export const fetchLibraryEntity: QueryFunction<IADSApiLibraryEntityResponse> = async ({ meta }) => {
+  const { params } = meta as { params: IADSApiLibraryEntityParams };
+  const config: ApiRequestConfig = {
+    method: 'GET',
+    url: `${ApiTargets.LIBRARIES}/${params.id}`,
+  };
+
+  const { data } = await api.request<IADSApiLibraryEntityResponse>(config);
+  return data;
+};
+
+// documents
+
+export const useDeleteLibrary: ADSMutation<IADSApiLibraryDeleteResponse, undefined, IADSApiLibraryDeleteParams> = (
+  _,
+  options,
+) => {
+  return useMutation({
+    mutationKey: librariesKeys.delete(),
+    mutationFn: deleteLibrary,
+    ...options,
+  });
+};
+
+export const deleteLibrary: MutationFunction<IADSApiLibraryDeleteResponse, IADSApiLibraryDeleteParams> = async ({
+  id,
+}) => {
+  const config: ApiRequestConfig = {
+    method: 'DELETE',
+    url: `${ApiTargets.DOCUMENTS}/${id}`,
+  };
+
+  const { data } = await api.request<IADSApiLibraryDeleteResponse>(config);
+  return data;
+};
+
+export const useEditLibraryMeta: ADSMutation<IADSApiLibraryEditMetaResponse, undefined, IADSApiLibraryEditMetaParams> =
+  (_, options) => {
+    return useMutation({
+      mutationKey: librariesKeys.edit(),
+      mutationFn: editLibraryMeta,
+      ...options,
+    });
+  };
+
+export const editLibraryMeta: MutationFunction<IADSApiLibraryEditMetaResponse, IADSApiLibraryEditMetaParams> = async (
+  params,
+) => {
+  const config: ApiRequestConfig = {
+    method: 'PUT',
+    url: `${ApiTargets.DOCUMENTS}/${params.id}`,
+    data: omit(['id'], params),
+  };
+
+  const { data } = await api.request<IADSApiLibraryEditMetaResponse>(config);
+  return data;
+};
+
+export const useEditLibraryDocuments: ADSMutation<
+  IADSApiLibraryDocumentResponse,
+  undefined,
+  IADSApiLibraryDocumentParams
+> = (_, options) => {
+  return useMutation({
+    mutationKey: librariesKeys.document(),
+    mutationFn: editLibraryDocuments,
+    ...options,
+  });
+};
+
+export const editLibraryDocuments: MutationFunction<IADSApiLibraryDocumentResponse, IADSApiLibraryDocumentParams> =
+  async (params) => {
+    const config: ApiRequestConfig = {
+      method: 'POST',
+      url: `${ApiTargets.DOCUMENTS}/${params.id}`,
+      data: omit(['id'], params),
+    };
+
+    const { data } = await api.request<IADSApiLibraryDocumentResponse>(config);
+    return data;
+  };
+
+// operations
+
+export const useLibraryOperation: ADSMutation<
+  IADSApiLibraryOperationResponse,
+  undefined,
+  IADSApiLibraryOperationParams
+> = (_, options) => {
+  return useMutation({
+    mutationKey: librariesKeys.operation(),
+    mutationFn: operation,
+    ...options,
+  });
+};
+
+export const operation: MutationFunction<IADSApiLibraryOperationResponse, IADSApiLibraryOperationParams> = async (
+  params,
+) => {
+  const config: ApiRequestConfig = {
+    method: 'POST',
+    url: `${ApiTargets.LIBRARY_OPERATION}/${params.id}`,
+    data: omit(['id'], params),
+  };
+
+  const { data } = await api.request<IADSApiLibraryOperationResponse>(config);
+  return data;
+};
+
+// query
+
+export const useAddDocumentsByQuery: ADSQuery<IADSApiLibraryQueryParams, IADSApiLibraryQueryResponse> = (
+  params,
+  options,
+) => {
+  return useQuery({
+    queryKey: librariesKeys.query(),
+    queryFn: addDocumentsByQuery,
+    meta: { params },
+    ...options,
+  });
+};
+
+export const addDocumentsByQuery: QueryFunction<IADSApiLibraryQueryResponse> = async ({ meta }) => {
+  const { params } = meta as { params: IADSApiLibraryQueryParams };
+  const config: ApiRequestConfig = {
+    method: 'GET',
+    url: `${ApiTargets.LIBRARY_QUERY}/${params.id}`,
+    params: omit(['id'], params),
+  };
+
+  const { data } = await api.request<IADSApiLibraryQueryResponse>(config);
+  return data;
+};
+
+export const useUpdateDocumentsByQuery: ADSMutation<
+  IADSApiLibraryQueryUpdateResponse,
+  undefined,
+  IADSApiLibraryQueryUpdateParams
+> = (_, options) => {
+  return useMutation({
+    mutationKey: librariesKeys.query_update(),
+    mutationFn: updateDocumentsByQuery,
+    ...options,
+  });
+};
+
+export const updateDocumentsByQuery: MutationFunction<
+  IADSApiLibraryQueryUpdateResponse,
+  IADSApiLibraryQueryUpdateParams
+> = async (params) => {
+  const config: ApiRequestConfig = {
+    method: 'POST',
+    url: `${ApiTargets.LIBRARY_QUERY}/${params.id}`,
+    data: omit(['id'], params),
+  };
+
+  const { data } = await api.request<IADSApiLibraryQueryUpdateResponse>(config);
+  return data;
+};
+
+// permissions
+
+export const useGetPermission: ADSQuery<IADSApiLibraryPermissionParams, IADSApiLibraryPermissionResponse> = (
+  params,
+  options,
+) => {
+  return useQuery({
+    queryKey: librariesKeys.permission(params),
+    queryFn: getPermission,
+    meta: { params },
+    ...options,
+  });
+};
+
+export const getPermission: QueryFunction<IADSApiLibraryPermissionResponse> = async ({ meta }) => {
+  const { params } = meta as { params: IADSApiLibraryPermissionParams };
+  const config: ApiRequestConfig = {
+    method: 'GET',
+    url: `${ApiTargets.PERMISSIONS}/${params.id}`,
+  };
+
+  const { data } = await api.request<IADSApiLibraryPermissionResponse>(config);
+  return data;
+};
+
+export const useModifyPermission: ADSMutation<
+  IADSApiLibraryPermissionUpdateResponse,
+  undefined,
+  IADSApiLibraryPermissionUpdateParams
+> = (_, options) => {
+  return useMutation({
+    mutationKey: librariesKeys.query_update(),
+    mutationFn: modifyPermission,
+    ...options,
+  });
+};
+
+export const modifyPermission: MutationFunction<
+  IADSApiLibraryPermissionUpdateResponse,
+  IADSApiLibraryPermissionUpdateParams
+> = async (params) => {
+  const config: ApiRequestConfig = {
+    method: 'POST',
+    url: `${ApiTargets.PERMISSIONS}/${params.id}`,
+    data: omit(['id'], params),
+  };
+
+  const { data } = await api.request<IADSApiLibraryPermissionUpdateResponse>(config);
+  return data;
+};
+
+// transfer
+
+export const useTransfer: ADSMutation<IADSApiLibraryTransferResponse, undefined, IADSApiLibraryTransferParams> = (
+  _,
+  options,
+) => {
+  return useMutation({
+    mutationKey: librariesKeys.transfer(),
+    mutationFn: transfer,
+    ...options,
+  });
+};
+
+export const transfer: MutationFunction<IADSApiLibraryTransferResponse, IADSApiLibraryTransferParams> = async (
+  params,
+) => {
+  const config: ApiRequestConfig = {
+    method: 'POST',
+    url: `${ApiTargets.PERMISSIONS}/${params.id}`,
+    data: omit(['library'], params),
+  };
+
+  const { data } = await api.request<IADSApiLibraryTransferResponse>(config);
+  return data;
+};
--- src/api/biblib/types.ts
+++ src/api/biblib/types.ts
@@ -1,7 +1,16 @@
-export interface ILibraryEntity {
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import { IADSApiSearchParams } from '@api/search';
+
+export type Permission = 'owner' | 'admin' | 'read' | 'write';
+
+export type UserPermission = {
+  [user in string]: Permission[];
+};
+
+export interface ILibraryMetadata {
   num_documents: number;
   date_last_modified: string;
-  permission: string;
+  permission: Permission;
   description: string;
   public: boolean;
   num_users: number;
@@ -11,20 +20,149 @@ export interface ILibraryEntity {
   name: string;
 }
 
-export interface ILibraryApiResponse {
-  libraries: ILibraryEntity[];
+// Get all libraries
+export interface IADSApiLibraryParams {
+  start: number;
+  rows: number;
+  sort_col: 'date_created' | 'date_last_modified'; // no sort by name?
+  sort_dir: 'asc' | 'desc';
 }
 
-export interface ILibraryApiErrorResponse {
-  error: string;
+export interface IADSApiLibraryResponse {
+  libraries: ILibraryMetadata[];
 }
 
-export interface ILibraryApiEntityResponse {
-  documents: unknown[];
+// Get library Entity
+export interface IADSApiLibraryEntityParams {
+  id: string;
+}
+export interface IADSApiLibraryEntityResponse {
+  documents: string[];
   updates: {
     update_list: unknown[];
     num_updated: 0;
     duplicates_removed: 0;
   };
-  metadata: ILibraryEntity;
+  metadata: ILibraryMetadata;
+}
+
+// Add library
+
+export interface IADSApiLibraryAddParams {
+  name: string;
+  description: string;
+  public: boolean;
+  bibcode?: string[];
+}
+
+export interface IADSApiLibraryAddResponse {
+  name: string;
+  id: string;
+  description: string;
+}
+
+// Delete library
+
+export interface IADSApiLibraryDeleteParams {
+  id: string;
+}
+
+export interface IADSApiLibraryDeleteResponse {}
+
+// Modify library metadata
+export interface IADSApiLibraryEditMetaParams {
+  id: string;
+  name?: string;
+  description?: string;
+  public?: boolean;
+}
+
+export interface IADSApiLibraryEditMetaResponse {
+  name?: string;
+  description?: string;
+  public?: boolean;
+}
+
+// Library operation
+export interface IADSApiLibraryOperationParams {
+  id: string;
+  libraries?: string[]; // List of secondary libraries to include in the action (optional, based on action)
+  action: 'union' | 'intersection' | 'difference' | 'copy' | 'empty';
+  name?: string; // name of the new library (must be unique for that user); used only for actions in [union, intersection, difference]
+  description?: string; // description of the new library; used only for actions in [union, intersection, difference]
+  public?: boolean; // is the new library public to view; used only for actions in [union, intersection, difference]
+}
+
+export interface IADSApiLibraryOperationResponse {
+  name: string;
+  id: string;
+  description: string;
+}
+
+// Add, remove document to/from library using bibcodes
+
+export interface IADSApiLibraryDocumentParams {
+  id: string;
+  bibcode: string[]; // List of bibcodes
+  action: 'add' | 'remove';
+}
+
+export interface IADSApiLibraryDocumentResponse {
+  number_added: number;
+  number_removed: number;
+}
+
+// Add documents to library using search query
+
+export interface IADSApiLibraryQueryParams extends IADSApiSearchParams {
+  id: string;
+}
+
+export interface IADSApiLibraryQueryResponse {
+  number_added: number; // number of documents added
+  valid_bibcode: string[]; // the list of valid bibcodes
+}
+
+// Add/remove documents to library using search query
+
+export interface IADSApiLibraryQueryUpdateParams {
+  id: string;
+  query: IADSApiSearchParams;
+  action: 'add' | 'remove';
+}
+
+export interface IADSApiLibraryQueryUpdateResponse {
+  number_added: number; // number of documents added (if 'add' is used)
+  number_removed: number; // number of documents removed (if 'remove' is used)
+  valid_bibcode: string[]; // the list of valid bibcodes
+}
+
+// permission
+
+export interface IADSApiLibraryPermissionParams {
+  id: string;
+}
+
+export interface IADSApiLibraryPermissionResponse extends Array<UserPermission> {}
+
+export interface IADSApiLibraryPermissionUpdateParams {
+  id: string;
+  email: string; // which user
+  permissions: Omit<Permission, 'owner'>; //specifies which permission to change
+  value: boolean; // whether the user has this permission
+}
+
+export interface IADSApiLibraryPermissionUpdateResponse {}
+
+// Transfer
+
+export interface IADSApiLibraryTransferParams {
+  id: string;
+  transfer_user: string; //  e-mail of the user the account should be transfered to
+}
+
+export interface IADSApiLibraryTransferResponse {}
+
+export interface IADSApiLibraryErrorResponse {
+  error: string;
 }
