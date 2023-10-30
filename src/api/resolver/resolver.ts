2e87477465b7deda57f246c0f009b06fc19779a7
Jennifer Chen <shinyi.chen@gmail.com>
2023-10-30 11:00:14 -0700

    SCIX-290 Add related materials in abstract

---
+++ src/api/resolver/resolver.ts
@@ -0,0 +1,34 @@
+import api, { ApiRequestConfig } from '@api/api';
+import { ApiTargets } from '@api/models';
+import { ADSQuery } from '@api/types';
+import { QueryFunction, useQuery } from '@tanstack/react-query';
+import { IADSApiResolverParams, IADSApiResolverResponse } from './types';
+
+export enum ResolverKeys {
+  LINKS = 'resolver/links',
+}
+
+export const resolverKeys = {
+  links: (params: IADSApiResolverParams) => [params] as const,
+};
+
+export const useResolverQuery: ADSQuery<IADSApiResolverParams, IADSApiResolverResponse> = (params, options) => {
+  return useQuery({
+    queryKey: resolverKeys.links(params),
+    queryFn: fetchLinks,
+    meta: { params },
+    ...options,
+  });
+};
+
+export const fetchLinks: QueryFunction<IADSApiResolverResponse> = async ({ meta }) => {
+  const { params } = meta as { params: IADSApiResolverParams };
+
+  const config: ApiRequestConfig = {
+    method: 'GET',
+    url: `${ApiTargets.RESOLVER}/${params.bibcode}/${params.link_type}`,
+  };
+
+  const { data } = await api.request<IADSApiResolverResponse>(config);
+  return data;
+};
