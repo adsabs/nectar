24cfe7ccdee832ee075f2765798275f17ccf42e1
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-11-01 03:52:26 -0400

    add analytics call for theme change

--- src/components/NavBar/ThemeDropdown.tsx
+++ src/components/NavBar/ThemeDropdown.tsx
@@ -5,10 +5,12 @@ import { Theme } from '@types';
 import { ReactElement, useMemo } from 'react';
 import shallow from 'zustand/shallow';
 import { themes } from './models';
+import { useGTMDispatch } from '@elgorditosalsero/react-gtm-hook';
 
 const options = Object.values(themes);
 
 export const ThemeDropdown = (): ReactElement => {
+  const sendDataToGTM = useGTMDispatch();
   const [theme, setTheme]: [Theme, (theme: Theme) => void] = useStore(
     (state) => [state.theme, state.setTheme],
     shallow,
@@ -16,13 +18,21 @@ export const ThemeDropdown = (): ReactElement => {
 
   const option = useMemo(() => themes[theme], [theme]);
 
+  const handleThemeChange = ({ id: theme }: SelectOption<Theme>) => {
+    setTheme(theme);
+    sendDataToGTM({
+      event: 'theme_change',
+      theme: theme,
+    });
+  };
+
   return (
     <Box width={{ base: '200px', xs: '270px' }}>
       <Select<SelectOption<Theme>>
         value={option}
         options={options}
         stylesTheme="theme"
-        onChange={({ id: theme }) => setTheme(theme)}
+        onChange={handleThemeChange}
         label="Select theme"
         id="theme-selector"
         instanceId="theme-selector"
