2ee45e1ac630578cfcb84418c83c1a172b149e0c
Jennifer Chen <shinyi.chen@gmail.com>
2023-11-07 14:03:32 -0800

    update favicon

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,13 @@
 import { Container, Flex, useMediaQuery } from '@chakra-ui/react';
 import { SkipNavLink } from '@chakra-ui/skip-nav';
-import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { Footer } from '../Footer';
 import { NavBar } from '../NavBar';
 import dynamic from 'next/dynamic';
 import { LandingTabsStatic } from '@components';
 import { Notification } from '@components/Notification';
+import Head from 'next/head';
 
 const LandingTabs = dynamic(() => import('@components/LandingTabs/LandingTabs').then((mod) => mod.LandingTabs), {
   ssr: false,
@@ -16,15 +16,26 @@ const LandingTabs = dynamic(() => import('@components/LandingTabs/LandingTabs').
 
 const LANDING_PAGES = ['/', '/classic-form', '/paper-form'];
 
+const darkModeFavicon = '/favicon-dark.png';
+
+const lightModeFavicon = '/favicon-light.png';
+
 export const Layout: FC = ({ children }) => {
   const router = useRouter();
+
   const isLandingPage = LANDING_PAGES.includes(router.pathname);
+
   const [isPrint] = useMediaQuery('print');
+
+  const [isDarkMode] = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const favicon = useMemo(() => (isDarkMode ? darkModeFavicon : lightModeFavicon), [isDarkMode]);
 
   return (
     <Flex direction="column">
       <Head>
         <title>NASA Science Explorer</title>
+        <link rel="icon" type="image/png" href={favicon} />
       </Head>
       <SkipNavLink id="main-content">Skip to content</SkipNavLink>
       {isPrint ? null : <NavBar />}
