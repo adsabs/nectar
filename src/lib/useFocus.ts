f02465afb8eb6350dcf66d29cb8f4f8eb42619a5
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-11-13 11:24:11 -0500

    Small changes to make search bar work properly
    Update focus hook to allow more options
    Debounce appending to query in intermediateQuery hook
    Raises responsibility of updating store to parent component.  The store query val will be grabbed initially, but otherwise is not touched by the searchbar until submission.

--- src/lib/useFocus.ts
+++ src/lib/useFocus.ts
@@ -5,6 +5,8 @@ interface IUseFocusProps {
 
   /** if element is an input, select the text on focus */
   selectTextOnFocus?: boolean;
+
+  moveCursorToEnd?: boolean;
 }
 
 /**
@@ -15,18 +17,27 @@ interface IUseFocusProps {
  * @param props
  */
 export const useFocus = <TElement extends HTMLElement = HTMLInputElement>(
-  props: IUseFocusProps = { focusOnMount: true, selectTextOnFocus: true },
-): [MutableRefObject<TElement>, () => void] => {
-  const { focusOnMount } = props;
+  props: IUseFocusProps = { focusOnMount: true, selectTextOnFocus: true, moveCursorToEnd: true },
+): [MutableRefObject<TElement>, (overrides?: IUseFocusProps) => void] => {
+  const { focusOnMount, selectTextOnFocus, moveCursorToEnd } = props;
   const ref = useRef<TElement>(null);
 
-  const focus = () => {
+  const focus = (overrides?: IUseFocusProps) => {
     if (typeof ref.current?.focus === 'function' && typeof window !== 'undefined') {
-      if (props.selectTextOnFocus && ref.current instanceof HTMLInputElement) {
+      const shouldSelectTextOnFocus = overrides?.selectTextOnFocus ?? selectTextOnFocus;
+      const shouldMoveCursorToEnd = overrides?.moveCursorToEnd ?? moveCursorToEnd;
+
+      // override takes precedence, otherwise use props
+      if (shouldSelectTextOnFocus && ref.current instanceof HTMLInputElement) {
         ref.current?.select();
       }
 
       ref.current?.focus();
+
+      // if the element is an input, move the cursor to the end
+      if (shouldMoveCursorToEnd && ref.current instanceof HTMLInputElement) {
+        ref.current.selectionStart = ref.current.value.length;
+      }
     }
   };
 
