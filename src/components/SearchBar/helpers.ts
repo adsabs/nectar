3ece8b6da701991b1b907af9f5a090299cea8760
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-11-13 11:19:00 -0500

    Remove downshift in favor of simple list

--- src/components/SearchBar/helpers.ts
+++ src/components/SearchBar/helpers.ts
@@ -13,6 +13,57 @@ export const filterItems = curry((query: string, items: TypeaheadOption[]) => {
 });
 
 export const extractFinalTerm = (query: string) => {
+  // detect fields, and return the last field
   const fields = query.match(/(?:[^\s"]+|"[^"]*")+/g);
-  return fields === null ? query : fields[fields.length - 1];
+
+  if (fields !== null && fields.length > 1) {
+    // if query is a field with no value, return empty string
+    if (query.endsWith(': ')) {
+      return '';
+    }
+
+    return fields[fields.length - 1];
+  }
+
+  // if query ends with a space, return empty string
+  if (query.endsWith(' ')) {
+    return '';
+  }
+
+  return query;
+};
+
+export const updateSearchTerm = (searchTerm: string, value: string) => {
+  return searchTerm ? `${searchTerm.replace(/\S+$/, '')}${value}` : value;
+};
+
+export const appendSearchTerm = (searchTerm: string, value: string) => {
+  return searchTerm.length > 0 ? `${searchTerm} ${value}` : value;
+};
+
+export const getCursorPosition = (searchTerm: string) => {
+  // if the final character in the search term is an empty set of quotes, parens, or brackets - move the cursor to the inside
+  if (
+    searchTerm.endsWith(`""`) ||
+    searchTerm.endsWith(`"^"`) ||
+    searchTerm.endsWith(`()`) ||
+    searchTerm.endsWith(`[]`)
+  ) {
+    return searchTerm.length - 1;
+  }
+  return searchTerm.length;
+};
+
+export const getFocusedItemValue = (items: TypeaheadOption[], focused: number) => {
+  if (focused === -1 || focused >= items.length) {
+    return null;
+  }
+  return items[focused].value;
+};
+
+export const getPreview = (searchTerm: string, value: string | null) => {
+  if (value === null) {
+    return searchTerm;
+  }
+  return updateSearchTerm(searchTerm, value);
 };
---
+++ src/components/SearchBar/searchInputReducer.ts
@@ -0,0 +1,155 @@
+import { TypeaheadOption } from '@components/SearchBar/types';
+import { Reducer } from 'react';
+import {
+  appendSearchTerm,
+  extractFinalTerm,
+  filterItems,
+  getCursorPosition,
+  getFocusedItemValue,
+  getPreview,
+  updateSearchTerm,
+} from '@components/SearchBar/helpers';
+import { typeaheadOptions } from '@components/SearchBar/models';
+
+export interface ISearchInputState {
+  isOpen: boolean;
+  searchTerm: string;
+  items: TypeaheadOption[];
+  focused: number;
+  cursorPosition: number;
+}
+
+export type SearchInputAction =
+  | { type: 'SET_SEARCH_TERM'; payload: string }
+  | { type: 'SET_SEARCH_TERM_ADDITION'; payload: string }
+  | { type: 'CLICK_ITEM' }
+  | { type: 'FOCUS_ITEM'; index: number }
+  | { type: 'KEYDOWN_ENTER' }
+  | { type: 'KEYDOWN_ESCAPE' }
+  | { type: 'HARD_RESET' }
+  | { type: 'SOFT_RESET' }
+  | { type: 'KEYDOWN_ARROW_DOWN' }
+  | { type: 'KEYDOWN_ARROW_UP' }
+  | { type: 'KEYDOWN_TAB' };
+
+export const reducer: Reducer<ISearchInputState, SearchInputAction> = (state, action) => {
+  switch (action.type) {
+    case 'SOFT_RESET':
+      return { ...initialState, searchTerm: state.searchTerm, cursorPosition: state.cursorPosition };
+    case 'HARD_RESET':
+      return initialState;
+    case 'SET_SEARCH_TERM': {
+      if (extractFinalTerm(action.payload) === '') {
+        return {
+          ...state,
+          isOpen: false,
+          searchTerm: action.payload,
+          focused: -1,
+          items: [],
+        };
+      }
+      const items = filterItems(extractFinalTerm(action.payload), typeaheadOptions);
+      return {
+        ...state,
+        isOpen: items.length > 0,
+        searchTerm: action.payload,
+        focused: -1,
+        items,
+      };
+    }
+    case 'SET_SEARCH_TERM_ADDITION': {
+      const searchTerm = appendSearchTerm(state.searchTerm, action.payload);
+      return {
+        ...state,
+        searchTerm,
+        preview: searchTerm,
+        cursorPosition: getCursorPosition(searchTerm),
+      };
+    }
+    case 'KEYDOWN_ARROW_DOWN': {
+      // if menu is closed, open menu and focus first item
+      if (!state.isOpen) {
+        // if final character is not a space, add a space
+        const searchTerm = state.searchTerm.endsWith(' ') ? state.searchTerm : appendSearchTerm(state.searchTerm, '');
+
+        return {
+          ...state,
+          isOpen: true,
+          focused: 0,
+          searchTerm,
+          preview: searchTerm,
+
+          // if no items, show all items
+          items: state.items.length === 0 ? typeaheadOptions : state.items,
+        };
+      }
+
+      // if menu is open, and we're at the bottom, cycle to the top
+      if (state.focused === state.items.length - 1) {
+        return {
+          ...state,
+          focused: -1,
+        };
+      }
+
+      return {
+        ...state,
+        focused: state.focused + 1,
+      };
+    }
+
+    case 'KEYDOWN_ARROW_UP': {
+      // if menu is open, and we're at the top, cycle to the bottom
+      if (state.focused === -1 && state.items.length > 0 && state.isOpen) {
+        return {
+          ...state,
+          focused: state.items.length - 1,
+        };
+      }
+
+      return {
+        ...state,
+        focused: state.focused - 1,
+      };
+    }
+
+    case 'KEYDOWN_TAB':
+    case 'CLICK_ITEM':
+    case 'KEYDOWN_ENTER': {
+      // if menu is open, and we're focused on an item, select it
+      if (state.focused > -1) {
+        const searchTerm = updateSearchTerm(state.searchTerm, state.items[state.focused].value);
+        return {
+          ...state,
+          isOpen: false,
+          focused: -1,
+          searchTerm: getPreview(searchTerm, getFocusedItemValue(state.items, state.focused)),
+          cursorPosition: getCursorPosition(searchTerm),
+        };
+      }
+
+      // if menu is closed, and we're not focused on an item, do nothing
+      return {
+        ...state,
+        isOpen: false,
+        focused: -1,
+      };
+    }
+
+    case 'KEYDOWN_ESCAPE':
+      return state.isOpen ? { ...state, isOpen: false, focused: -1 } : state;
+
+    case 'FOCUS_ITEM':
+      return { ...state, focused: action.index };
+
+    default:
+      return state;
+  }
+};
+export const initialState: ISearchInputState = {
+  isOpen: false,
+  searchTerm: '',
+  items: [],
+  focused: -1,
+  cursorPosition: 0,
+};
