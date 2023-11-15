133f5360348432a5360a99fe0ed9ad325b8ae48e
Jennifer Chen <shinyi.chen@gmail.com>
2023-11-15 15:04:31 -0800

    correct library modified date

--- src/components/Libraries/LibraryListTable.tsx
+++ src/components/Libraries/LibraryListTable.tsx
@@ -338,8 +338,8 @@ export const LibraryListTable = (props: ILibraryListTableProps) => {
 };
 
 const LastModified = ({ date }: { date: string }) => {
-  const dateStr = new Date(date);
-  const formatted = intlFormatDistance(new Date(date), new Date());
+  const dateStr = new Date(`${date.match(/[+]([0-9]){2}:([0-9]){2}$/) ? date : `${date}+00:00`}`); // date string here is missing the timezone, add it or the time is wrong
+  const formatted = intlFormatDistance(dateStr, new Date());
   return (
     <Tooltip
       label={intlFormat(dateStr, {
