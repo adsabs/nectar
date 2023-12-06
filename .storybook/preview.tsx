4ac0a9fdff238130063119033bbf820b9633615e
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-12-06 15:53:59 -0500

    Fix storybook queryclient defaults

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,11 +1,12 @@
 import { Preview } from '@storybook/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { StoreProvider, useCreateStore } from '../src/store/store';
 import { initialize, mswLoader } from 'msw-storybook-addon';
 import { handlers } from '../src/mocks/handlers';
 import { theme } from '../src/theme';
 import { MathJaxProvider } from '../src/mathjax';
 import { GTMProvider } from '@elgorditosalsero/react-gtm-hook';
+import { useCreateQueryClient } from '@lib';
 
 export const parameters = {
   chakra: { theme },
@@ -15,14 +16,22 @@ export const parameters = {
 // start msw server
 initialize();
 
-const queryClient = new QueryClient({ defaultOptions: { queries: { retry: 0 } } });
 const preview: Preview = {
   decorators: [
     (Story) => {
+      const qc = useCreateQueryClient();
+      qc.setDefaultOptions({
+        queries: {
+          refetchOnWindowFocus: false,
+          refetchOnReconnect: false,
+          refetchOnMount: false,
+          retry: false,
+        },
+      });
       return (
         <GTMProvider>
           <MathJaxProvider>
-            <QueryClientProvider client={queryClient}>
+            <QueryClientProvider client={qc}>
               <StoreProvider createStore={useCreateStore({})}>
                 <Story />
               </StoreProvider>
