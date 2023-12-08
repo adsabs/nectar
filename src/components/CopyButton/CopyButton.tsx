32f1be93060afc9715c61fc4644be00b56848f9a
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-12-08 13:28:05 -0500

    support selecting position of icon on copybutton

--- src/components/CopyButton/CopyButton.tsx
+++ src/components/CopyButton/CopyButton.tsx
@@ -6,6 +6,7 @@ export interface ICopyButtonProps extends ButtonProps {
   text: string;
   onCopyComplete?: () => void;
   options?: UseClipboardOptions;
+  iconPos?: 'left' | 'right';
 }
 
 export const SimpleCopyButton = (props: ICopyButtonProps): ReactElement => {
@@ -23,20 +24,18 @@ export const SimpleCopyButton = (props: ICopyButtonProps): ReactElement => {
   }, [hasCopied]);
 
   return (
-    <>
-      <Tooltip label={hasCopied ? 'Copied' : 'Copy to clipboard'}>
-        {hasCopied ? (
-          <IconButton aria-label="copied" icon={<CheckIcon />} variant="link" color="green.500" {...rest} />
-        ) : (
-          <IconButton icon={<CopyIcon />} variant="link" aria-label="copy to clipboard" onClick={onCopy} {...rest} />
-        )}
-      </Tooltip>
-    </>
+    <Tooltip label={hasCopied ? 'Copied' : 'Copy to clipboard'}>
+      {hasCopied ? (
+        <IconButton aria-label="copied" icon={<CheckIcon />} variant="link" color="green.500" {...rest} />
+      ) : (
+        <IconButton icon={<CopyIcon />} variant="link" aria-label="copy to clipboard" onClick={onCopy} {...rest} />
+      )}
+    </Tooltip>
   );
 };
 
 export const LabeledCopyButton = (props: ICopyButtonProps & { label: string }): ReactElement => {
-  const { label, text, options, onCopyComplete, ...rest } = props;
+  const { label, text, options, onCopyComplete, iconPos = 'left', ...rest } = props;
   const { hasCopied, onCopy, setValue } = useClipboard(text, options);
 
   useEffect(() => {
@@ -50,10 +49,14 @@ export const LabeledCopyButton = (props: ICopyButtonProps & { label: string }): 
   }, [hasCopied]);
 
   return (
-    <>
-      <Button leftIcon={<CopyIcon />} variant="link" aria-label="copy to clipboard" onClick={onCopy} {...rest}>
-        {hasCopied ? 'Copied to clipboard!' : label}
-      </Button>
-    </>
+    <Button
+      variant="link"
+      aria-label="copy to clipboard"
+      onClick={onCopy}
+      {...(iconPos === 'left' ? { leftIcon: <CopyIcon /> } : { rightIcon: <CopyIcon /> })}
+      {...rest}
+    >
+      {hasCopied ? 'Copied to clipboard!' : label}
+    </Button>
   );
 };
