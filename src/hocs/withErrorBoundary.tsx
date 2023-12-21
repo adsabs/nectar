67c7adc1aae57c1ff0ac1841776e943c09e28d8d
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2023-12-21 16:47:52 -0500

    add withErrorBoundary HOC

---
+++ src/hocs/withErrorBoundary.tsx
@@ -0,0 +1,30 @@
+import { FunctionComponent, ReactNode, Suspense } from 'react';
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+
+interface IWithErrorBoundaryProps {
+  onReset?: () => void;
+  onErrorRender: (fallbackProps?: FallbackProps) => ReactNode;
+  onLoadingRender: () => ReactNode;
+}
+
+export const withErrorBoundary =
+  <TProps extends object>(options: IWithErrorBoundaryProps, WrappedComponent: FunctionComponent<TProps>) =>
+  (props: TProps) =>
+    (
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary
+            onReset={() => {
+              options?.onReset?.();
+              reset();
+            }}
+            fallbackRender={options.onErrorRender}
+          >
+            <Suspense fallback={options.onLoadingRender()}>
+              <WrappedComponent {...props} />
+            </Suspense>
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
+    );
