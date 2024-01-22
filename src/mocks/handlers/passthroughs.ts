3771a448431d5fd098b4174dd0465a99cc289c88
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2024-01-22 14:40:43 -0500

    Add Sentry.io Integration

--- src/mocks/handlers/passthroughs.ts
+++
@@ -1,8 +0,0 @@
-import { rest } from 'msw';
-
-export const passthroughs = [
-  rest.all('/_next/*', (req) => req.passthrough()),
-  rest.all('__next*', (req) => req.passthrough()),
-  rest.get('/site.webmanifest', (req) => req.passthrough()),
-  rest.get('/favicon*', (req) => req.passthrough()),
-];
