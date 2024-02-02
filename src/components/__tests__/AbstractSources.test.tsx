9424691596a310dce8b75c6d52d372064186c0e2
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2024-02-02 14:19:23 -0500

    fix broken test
    Needed to mock the next/router functionality since it's now being used
    inside the useSession hook.

--- src/components/__tests__/AbstractSources.test.tsx
+++ src/components/__tests__/AbstractSources.test.tsx
@@ -1,9 +1,16 @@
 import { render } from '@test-utils';
 import Meta, { Default } from '../__stories__/AbstractSources.stories';
 import { composeStory } from '@storybook/react';
-import { test } from 'vitest';
+import { test, vi } from 'vitest';
 
 const AbstractSources = composeStory(Default, Meta);
+
+vi.mock('next/router', () => ({
+  useRouter: () => ({
+    reload: vi.fn(),
+  }),
+}));
+
 test('renders without crashing', () => {
   render(<AbstractSources />);
 });
