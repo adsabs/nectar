e64facf0f789ba3b5f839a41b1e5dcd9bdf5048e
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2024-03-18 14:38:43 -0400

    fix favicon color scheme issue

--- src/components/Favicons/Favicons.tsx
+++ src/components/Favicons/Favicons.tsx
@@ -1,28 +1,50 @@
-import { useMediaQuery } from '@chakra-ui/react';
-
 export const Favicons = () => {
-  const [systemDarkMode] = useMediaQuery('(prefers-color-scheme: dark)', { ssr: true, fallback: false });
-
-  if (systemDarkMode) {
-    // light-colored icon (for dark mode)
-    return (
-      <>
-        <link rel="icon" type="image/png" sizes="32x32" href="/dark/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/dark/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/dark/apple-touch-icon.png" />
-        <link rel="manifest" href="/dark/site.webmanifest" />
-        <meta name="theme-color" content="#ffffff" />
-      </>
-    );
-  }
-
-  // dark-colored icon (for light mode)
   return (
     <>
-      <link rel="icon" type="image/png" sizes="32x32" href="/light/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/light/favicon-16x16.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/light/apple-touch-icon.png" />
-      <link rel="manifest" href="/light/site.webmanifest" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/light/favicon-32x32.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/light/favicon-16x16.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/light/apple-touch-icon.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link rel="manifest" href="/light/site.webmanifest" media="(prefers-color-scheme: dark)" />
+
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/dark/favicon-32x32.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/dark/favicon-16x16.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/dark/apple-touch-icon.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link rel="manifest" href="/dark/site.webmanifest" media="(prefers-color-scheme: light)" />
+
       <meta name="theme-color" content="#ffffff" />
     </>
   );
