85ce38f067ef059ef0bafe387fb4f984c1a1fd42
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2024-03-27 15:40:53 -0400

    Remove heading from search examples placeholder

--- src/components/SearchExamples/SearchExamplesPlaceholder.tsx
+++ src/components/SearchExamples/SearchExamplesPlaceholder.tsx
@@ -1,4 +1,4 @@
-import { Flex, Grid, GridItem, Heading } from '@chakra-ui/react';
+import { Flex, Grid, GridItem } from '@chakra-ui/react';
 import { examples } from './examples';
 import { SearchExample } from './SearchExamples';
 
@@ -6,9 +6,6 @@ import { SearchExample } from './SearchExamples';
 export const SearchExamplesPlaceholder = () => {
   return (
     <Flex justifyContent="center" direction="column" alignItems="center">
-      <Heading as="h3" size="md" mt={3} mb={5}>
-        Search Examples
-      </Heading>
       <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }} gap={5}>
         <GridItem>
           {examples['GENERAL'].left.map(({ label, text }) => (
