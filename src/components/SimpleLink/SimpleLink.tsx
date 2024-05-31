d93c83ff60cee2f890641a1e2516afba261fd4ac
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2024-05-31 00:06:49 -0400

    Bugfixing
    Fix unique key error on abstract page
    
    Another unique key fix and formatting

--- src/components/SimpleLink/SimpleLink.tsx
+++ src/components/SimpleLink/SimpleLink.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, ReactElement } from 'react';
+import { PropsWithChildren, ReactElement, forwardRef, Ref } from 'react';
 import { Url } from 'next/dist/shared/lib/router/router';
 import { Link, LinkProps } from '@chakra-ui/next-js';
 
@@ -7,24 +7,27 @@ export interface ISimpleLinkProps extends LinkProps {
   icon?: ReactElement;
 }
 
-export const SimpleLink = (props: PropsWithChildren<ISimpleLinkProps>): ReactElement => {
-  const { children, icon, variant = 'default', prefetch = false, newTab, ...linkProps } = props;
-  const isExternal = isHrefExternal(linkProps.href, linkProps.isExternal);
+export const SimpleLink = forwardRef(
+  (props: PropsWithChildren<ISimpleLinkProps>, ref: Ref<HTMLAnchorElement>): ReactElement => {
+    const { children, icon, variant = 'default', prefetch = false, newTab, ...linkProps } = props;
+    const isExternal = isHrefExternal(linkProps.href, linkProps.isExternal);
 
-  return (
-    <Link
-      variant={variant}
-      isExternal={isExternal}
-      prefetch={prefetch}
-      rel={isExternal ? 'noopener noreferrer' : undefined}
-      target={typeof newTab === 'boolean' && newTab ? '_blank' : linkProps.target}
-      {...linkProps}
-    >
-      {icon && <>{icon}</>}
-      {children}
-    </Link>
-  );
-};
+    return (
+      <Link
+        ref={ref}
+        variant={variant}
+        isExternal={isExternal}
+        prefetch={prefetch}
+        rel={isExternal ? 'noopener noreferrer' : undefined}
+        target={typeof newTab === 'boolean' && newTab ? '_blank' : linkProps.target}
+        {...linkProps}
+      >
+        {icon && <>{icon}</>}
+        {children}
+      </Link>
+    );
+  },
+);
 
 const isHrefExternal = (href: Url, isExternal?: boolean): boolean =>
-  isExternal ?? /^https?/.test(typeof href === 'string' ? href : href.protocol);
+  isExternal ?? /^https?:/.test(typeof href === 'string' ? href : href.protocol);
