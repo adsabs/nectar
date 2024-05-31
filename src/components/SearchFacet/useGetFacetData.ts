0217388a7544205d28d6e219bdeeb5da2e80c433
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2024-05-31 14:14:45 -0400

    fix import in orcid workstable

--- src/components/SearchFacet/useGetFacetData.ts
+++ src/components/SearchFacet/useGetFacetData.ts
@@ -4,7 +4,7 @@ import { getLevelFromKey, getPrevKey } from '@/components/SearchFacet/helpers';
 import { useFacetStore } from '@/components/SearchFacet/store/FacetStore';
 import { FacetItem } from '@/components/SearchFacet/types';
 import { AppState, useStore } from '@/store';
-import { sanitize } from 'dompurify';
+import { sanitize } from 'isomorphic-dompurify';
 import { isEmpty, omit } from 'ramda';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { isNonEmptyArray, isNonEmptyString } from 'ramda-adjunct';
