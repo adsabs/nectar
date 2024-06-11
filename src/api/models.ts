570bf64dfca9d9cfff202dfbf370334c57ce4163
Jennifer Chen <shinyi.chen@gmail.com>
2024-06-11 14:43:37 -0400

    journals api

--- src/api/models.ts
+++ src/api/models.ts
@@ -248,4 +248,6 @@ export enum ApiTargets {
   DOCUMENTS = '/biblib/documents',
   PERMISSIONS = '/biblib/permissions',
   REFERENCE = '/reference/text',
+  JOURNAL = 'journals/journal',
+  JOURNAL_SUMMARY = 'journals/summary',
 }
