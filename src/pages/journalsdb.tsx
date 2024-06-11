57de16cb701f9f6c323b8f3fda7b9e04aaddb725
Jennifer Chen <shinyi.chen@gmail.com>
2024-06-11 16:17:17 -0400

    search by issn

--- src/pages/journalsdb.tsx
+++ src/pages/journalsdb.tsx
@@ -1,16 +1,22 @@
-import { useGetJournal, useGetJournalSummary } from '@/api/journals/journals';
+import { useGetISSN, useGetJournal, useGetJournalSummary } from '@/api/journals/journals';
 import { useDebounce } from '@/lib/useDebounce';
+import { makeSearchParams } from '@/utils';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import {
   Box,
   Button,
   CloseButton,
   Container,
+  FormControl,
+  FormLabel,
   IconButton,
   Input,
   InputGroup,
   InputRightElement,
+  Radio,
+  RadioGroup,
   Spinner,
+  Stack,
   Table,
   TableContainer,
   Tbody,
@@ -26,6 +32,33 @@ import { useRouter } from 'next/router';
 import { ChangeEvent, useMemo, useState } from 'react';
 
 const AbstractPage: NextPage = () => {
+  const [searchType, setSearchType] = useState('journal');
+
+  return (
+    <>
+      <Head>
+        <title>Journals Database</title>
+      </Head>
+      <Container maxW="container.xl" my={4} minH="container.sm">
+        <FormControl>
+          <Stack direction="row">
+            <FormLabel>Search by: </FormLabel>
+            <RadioGroup value={searchType} onChange={setSearchType}>
+              <Stack direction="row">
+                <Radio value="journal">Journal</Radio>
+                <Radio value="issn">ISSN</Radio>
+              </Stack>
+            </RadioGroup>
+          </Stack>
+        </FormControl>
+
+        {searchType === 'journal' ? <JournalSearch /> : <IssnSearch />}
+      </Container>
+    </>
+  );
+};
+
+const JournalSearch = () => {
   const router = useRouter();
 
   const [term, setTerm] = useState('');
@@ -41,62 +74,59 @@ const AbstractPage: NextPage = () => {
   };
 
   const searchByBibstem = (bibstem: string) => {
-    void router.push({ pathname: '/search', query: { q: `bibstem:"${bibstem}"` } });
+    void router.push({
+      pathname: '/search',
+      query: makeSearchParams({ q: `bibstem:"${bibstem}"`, sort: ['date desc'] }),
+    });
   };
 
   return (
     <>
-      <Head>
-        <title>Journals Database</title>
-      </Head>
-
-      <Container maxW="container.xl" my={4} minH="container.sm">
-        <InputGroup mt={4}>
-          <Input onChange={handleTermChange} value={term} placeholder="type in journal search term" autoFocus />
-          <InputRightElement>
-            <IconButton icon={<CloseButton />} aria-label="Clear search" onClick={() => setTerm('')} />
-          </InputRightElement>
-        </InputGroup>
-        <Box as="section" mt={4}>
-          {isFetching && <Spinner />}
-          {bibstem && <JournalSummary bibstem={bibstem} onClose={() => setBibstem(null)} />}
-          {!bibstem && !isFetching && data && (
-            <>
-              <Text size="sm" my={4}>
-                <strong>{data.journal.length}</strong> results
-              </Text>
-              {data.journal.length > 0 && (
-                <TableContainer>
-                  <Table>
-                    <Thead>
+      <InputGroup mt={4}>
+        <Input onChange={handleTermChange} value={term} placeholder="type in journal search term" autoFocus />
+        <InputRightElement>
+          <IconButton icon={<CloseButton />} aria-label="Clear search" onClick={() => setTerm('')} />
+        </InputRightElement>
+      </InputGroup>
+      <Box as="section" mt={4}>
+        {isFetching && <Spinner />}
+        {bibstem && <JournalSummary bibstem={bibstem} onClose={() => setBibstem(null)} />}
+        {!bibstem && !isFetching && data && (
+          <>
+            <Text size="sm" my={4}>
+              <strong>{data.journal.length}</strong> results
+            </Text>
+            {data.journal.length > 0 && (
+              <TableContainer>
+                <Table>
+                  <Thead>
+                    <Tr>
+                      <Th>Bibstem</Th>
+                      <Th>Name</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {data.journal.map(({ bibstem, name }) => (
                       <Tr>
-                        <Th>Bibstem</Th>
-                        <Th>Name</Th>
+                        <Td>
+                          <Button variant="link" onClick={() => searchByBibstem(bibstem)}>
+                            {bibstem}
+                          </Button>
+                        </Td>
+                        <Td>
+                          <Button variant="link" onClick={() => setBibstem(bibstem)}>
+                            {name}
+                          </Button>
+                        </Td>
                       </Tr>
-                    </Thead>
-                    <Tbody>
-                      {data.journal.map(({ bibstem, name }) => (
-                        <Tr>
-                          <Td>
-                            <Button variant="link" onClick={() => searchByBibstem(bibstem)}>
-                              {bibstem}
-                            </Button>
-                          </Td>
-                          <Td>
-                            <Button variant="link" onClick={() => setBibstem(bibstem)}>
-                              {name}
-                            </Button>
-                          </Td>
-                        </Tr>
-                      ))}
-                    </Tbody>
-                  </Table>
-                </TableContainer>
-              )}
-            </>
-          )}
-        </Box>
-      </Container>
+                    ))}
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            )}
+          </>
+        )}
+      </Box>
     </>
   );
 };
@@ -121,6 +151,46 @@ const JournalSummary = ({ bibstem, onClose }: { bibstem: string; onClose: () => 
               <strong>{data.summary.master.bibstem}</strong> {data.summary.master.journal_name}
             </Text>
             <pre>{summary}</pre>
+          </>
+        )}
+      </Box>
+    </>
+  );
+};
+
+const IssnSearch = () => {
+  const [term, setTerm] = useState('');
+
+  const { data, isFetching, error } = useGetISSN({ issn: term }, { enabled: term.length === 9 && term[4] === '-' });
+
+  const handleTermChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTerm(e.target.value);
+  };
+
+  return (
+    <>
+      <InputGroup mt={4}>
+        <Input onChange={handleTermChange} value={term} placeholder="type in issn" autoFocus />
+        <InputRightElement>
+          <IconButton icon={<CloseButton />} aria-label="Clear search" onClick={() => setTerm('')} />
+        </InputRightElement>
+      </InputGroup>
+      <Box as="section" mt={4}>
+        {isFetching && <Spinner />}
+        {!isFetching && !error && data?.issn && (
+          <>
+            <Text fontSize="2xl" my={2} fontWeight="bold">
+              ISSN: {data.issn.ISSN}
+            </Text>
+            <pre>{JSON.stringify(data.issn, null, 2)}</pre>
+          </>
+        )}
+        {!isFetching && !error && data && !data.issn && (
+          <>
+            <Text fontSize="2xl" my={2} fontWeight="bold">
+              Error
+            </Text>
+            <Text>ISSN not found</Text>
           </>
         )}
       </Box>
