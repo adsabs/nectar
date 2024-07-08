3bd4c5610b633984b1e544f97f808f5ca9f87c62
Jennifer Chen <shinyi.chen@gmail.com>
2024-07-08 11:00:12 -0700

    Fix abstract query page param

--- src/lib/useGetAbstractParams.ts
+++ src/lib/useGetAbstractParams.ts
@@ -1,3 +1,4 @@
+import { AppState, useStore } from '@/store';
 import { useCallback, useEffect, useRef, useState } from 'react';
 
 /**
@@ -5,12 +6,17 @@ import { useCallback, useEffect, useRef, useState } from 'react';
  */
 export const useGetAbstractParams = (id: string) => {
   const [start, setStart] = useState(0);
+  const pageSize = useStore((state: AppState) => state.numPerPage);
+
   const prev = useRef<string>();
   useEffect(() => {
     prev.current = id;
   });
 
   // reset start if the previous bibcode is different than the current
-  const getParams = useCallback(() => ({ bibcode: id, start: prev.current === id ? start : 0 }), [id, start]);
+  const getParams = useCallback(
+    () => ({ bibcode: id, start: prev.current === id ? start * pageSize : 0 }),
+    [id, start, pageSize],
+  );
   return { getParams, onPageChange: setStart };
 };
