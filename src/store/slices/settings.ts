d51de8b25ecc4c30b86c7893bf15300e955636c3
Jennifer Chen <shinyi.chen@gmail.com>
2025-03-19 10:33:12 -0700

    SCIX-528 hide/show planetary feature facet based on discipline

--- src/store/slices/settings.ts
+++ src/store/slices/settings.ts
@@ -200,9 +200,10 @@ export const settingsSlice: StoreSlice<ISettingsState & ISettingsAction> = (set,
 const getIgnoredSearchFacets = (mode: AppMode): SearchFacetID[] => {
   switch (mode) {
     case AppMode.EARTH_SCIENCE:
-      return ['simbad', 'ned', 'vizier'];
-
-    default:
+      return ['simbad', 'ned', 'vizier', 'planetary'];
+    case AppMode.PLANET_SCIENCE:
       return [];
+    default:
+      return ['planetary'];
   }
 };
