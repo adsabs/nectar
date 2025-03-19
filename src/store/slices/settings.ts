8270061cd7fa0221eabcfeedea51c6ee6e687e35
Jennifer Chen <shinyi.chen@gmail.com>
2025-03-19 10:36:25 -0700

    SCIX-526 hide/show uat facet based on discipline

--- src/store/slices/settings.ts
+++ src/store/slices/settings.ts
@@ -200,10 +200,12 @@ export const settingsSlice: StoreSlice<ISettingsState & ISettingsAction> = (set,
 const getIgnoredSearchFacets = (mode: AppMode): SearchFacetID[] => {
   switch (mode) {
     case AppMode.EARTH_SCIENCE:
-      return ['simbad', 'ned', 'vizier', 'planetary'];
+      return ['simbad', 'ned', 'vizier', 'planetary', 'uat'];
     case AppMode.PLANET_SCIENCE:
-      return [];
-    default:
+      return ['uat'];
+    case AppMode.ASTROPHYSICS:
       return ['planetary'];
+    default:
+      return ['planetary', 'uat'];
   }
 };
