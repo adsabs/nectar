e6a7fbe09a0d6e356063ef4ef39f1d6c289bf9af
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2025-07-28 15:29:00 -0400

    fix tests

--- src/components/SearchFacet/SearchFacetModal/AlphaSorter.tsx
+++ src/components/SearchFacet/SearchFacetModal/AlphaSorter.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, FlexProps, RadioProps, useRadio, useRadioGroup } from '@chakra-ui/react';
+import { Box, chakra, Flex, FlexProps, RadioProps, useRadio, useRadioGroup } from '@chakra-ui/react';
 import { range } from 'ramda';
 import { FC } from 'react';
 
@@ -24,35 +24,27 @@ export const AlphaSorter: FC<IAlphaSorterProps> = (props) => {
   );
 };
 
-const LetterRadio = (props: RadioProps) => {
-  const { getInputProps, getCheckboxProps } = useRadio(props);
-  const input = getInputProps();
-  const checkbox = getCheckboxProps();
+const LetterRadio = (props: Omit<RadioProps, 'onBeforeInput'>) => {
+  const { getInputProps, htmlProps, getRadioProps, state, getLabelProps } = useRadio(props);
 
   return (
-    <Box as="label">
-      <input {...input} />
+    <chakra.label {...htmlProps} cursor="pointer">
+      <input {...getInputProps({})} hidden />
       <Box
-        {...checkbox}
+        {...getRadioProps()}
         cursor="pointer"
         borderWidth="1px"
         borderRadius="none"
-        _disabled={{
-          bg: 'gray.400',
-        }}
-        _checked={{
-          bg: 'blue.600',
-          color: 'white',
-          borderColor: 'gray.200',
-        }}
-        _focus={{
-          boxShadow: 'outline',
-        }}
+        bg={state.isChecked ? 'blue.600' : 'transparent'}
+        color={state.isChecked ? 'white' : 'gray.600'}
+        borderColor={state.isChecked ? 'gray.200' : 'gray.300'}
+        _disabled={{ bg: 'gray.400' }}
+        _focus={{ boxShadow: 'outline' }}
         py="0.5"
         px="2"
       >
-        {props.value}
+        <Box {...getLabelProps()}>{props.value}</Box>
       </Box>
-    </Box>
+    </chakra.label>
   );
 };
