9643b08b42e2965672d18d8b537d1993872965ea
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2025-12-19 13:55:15 -0500

    fix: add cdn.jsdelivr.net to CSP font-src directive
    - MathJax fonts from cdn.jsdelivr.net were being blocked
    - Added https://cdn.jsdelivr.net to font-src to allow MathJax WOFF fonts

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const CSP = `
   base-uri 'self';
   object-src 'none';
   connect-src 'self' https://*.google-analytics.com https://*.adsabs.harvard.edu https://o1060269.ingest.sentry.io https://scixplorer.org https://*.scixplorer.org https://www.googletagmanager.com https://www.google.com https://recaptcha.google.com https://www.recaptcha.net https://recaptcha.net https://www.gstatic.com https://www.gstatic.cn https://*.googleapis.com https://*.clients6.google.com https://cdn.jsdelivr.net;
-  font-src 'self' data: https://cdnjs.cloudflare.com https://fonts.gstatic.com;
+  font-src 'self' data: https://cdnjs.cloudflare.com https://fonts.gstatic.com https://cdn.jsdelivr.net;
   frame-src https://www.youtube-nocookie.com https://www.google.com https://www.google.com/recaptcha/ https://recaptcha.google.com https://www.recaptcha.net https://recaptcha.net;
   form-action 'self';
   img-src * data: https://www.google.com https://www.gstatic.com https://i.ytimg.com https://s.ytimg.com;
