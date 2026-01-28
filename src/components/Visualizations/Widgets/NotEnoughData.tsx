dcc649356b566ae32d8f75ba8c5e4e11b1c5ad9a
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2026-01-28 13:58:42 -0500

    fix typo in viz error message

--- src/components/Visualizations/Widgets/NotEnoughData.tsx
+++ src/components/Visualizations/Widgets/NotEnoughData.tsx
@@ -8,9 +8,9 @@ export const NotEnoughData = (): ReactElement => {
     <Box>
       <Text>
         The network grouping algorithm could not generate group data for your network. This might be because the list of
-        papers was too small or sparse to produce multiple meaningful groups.
+        papers was too small or sparse to produce multiple meaningful groups.{' '}
         <SimpleLink href="/help/actions/visualize" newTab>
-          Learn more about how ADS networks are created and how they can be used.
+          Learn more about how SciX networks are created and how they can be used.
         </SimpleLink>
       </Text>
     </Box>
