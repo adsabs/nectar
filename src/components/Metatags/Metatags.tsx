91e13d087b618ea2679d56592cebff5ac3237603
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2026-06-08 23:47:29 -0400

    SCIX-877 feat(metatags): gate Google Scholar tags by doctype
    Only an allowed whitelist of doctypes emits Google Scholar-compatible
    meta tags (Highwire citation_*, PRISM prism.*, Dublin Core dc.*) on the
    abstract page. Other or unknown doctypes omit them; Schema.org JSON-LD,
    Open Graph, Twitter, canonical, and description tags remain for all
    records. Doctype comparisons are normalized to lowercase.

--- src/components/Metatags/Metatags.tsx
+++ src/components/Metatags/Metatags.tsx
@@ -2,6 +2,7 @@ import { Fragment, ReactElement } from 'react';
 import { getFormattedNumericPubdate, getFormattedCitationDate, stripHtml } from '@/utils/common/formatters';
 import { Esources, IDocsEntity } from '@/api/search/types';
 import { docToJsonld } from '@/components/Metatags/json-ld-abstract/docToJsonld';
+import { showsGoogleScholarTags } from '@/components/Metatags/scholarDoctypes';
 
 const baseUrl = process.env.BASE_CANONICAL_URL ?? '';
 const LINKGATEWAY_BASE_URL = `${baseUrl}/link_gateway`;
@@ -43,6 +44,11 @@ export const Metatags = (props: IMetatagsProps): ReactElement => {
 
   const authors = doc.author ? doc.author.slice(0, 50) : [];
 
+  // Google Scholar-compatible tags (Highwire/PRISM/Dublin Core) are gated
+  // by an allowed-doctype whitelist; Schema.org JSON-LD is emitted for all.
+  const normalizedDoctype = doc.doctype?.toLowerCase();
+  const showScholarTags = showsGoogleScholarTags(doc.doctype);
+
   return (
     <>
       <script
@@ -70,89 +76,101 @@ export const Metatags = (props: IMetatagsProps): ReactElement => {
 
       {doc.author && authors.map((a, i) => <meta key={`aa-${i}`} name="article:author" content={a} />)}
 
-      {doc.doctype === 'Proceedings' ? (
-        doc.bibstem &&
-        doc.bibstem.length > 0 && <meta name="citation_conference" content={doc.bibstem[0]} data-highwire="true" />
-      ) : doc.pub_raw ? (
-        <meta name="citation_journal_title" content={doc.pub_raw} data-highwire="true" />
-      ) : doc.pub ? (
-        <meta name="citation_journal_title" content={doc.pub} data-highwire="true" />
-      ) : null}
+      {showScholarTags && (
+        <>
+          {normalizedDoctype === 'proceedings' ? (
+            doc.bibstem &&
+            doc.bibstem.length > 0 && <meta name="citation_conference" content={doc.bibstem[0]} data-highwire="true" />
+          ) : doc.pub_raw ? (
+            <meta name="citation_journal_title" content={doc.pub_raw} data-highwire="true" />
+          ) : doc.pub ? (
+            <meta name="citation_journal_title" content={doc.pub} data-highwire="true" />
+          ) : null}
 
-      {doc.pubdate && <meta name="citation_date" content={citation_date} data-highwire="true" />}
+          {doc.pubdate && <meta name="citation_date" content={citation_date} data-highwire="true" />}
 
-      {doc.author &&
-        authors.map((author, i) => (
-          <Fragment key={`author-${i}`}>
-            <meta name="citation_author" content={author} data-highwire="true" />
-            {doc.aff && doc.aff[i] && (
-              <meta name="citation_author_institution" content={doc.aff[i]} data-highwire="true" />
-            )}
-          </Fragment>
-        ))}
+          {doc.author &&
+            authors.map((author, i) => (
+              <Fragment key={`author-${i}`}>
+                <meta name="citation_author" content={author} data-highwire="true" />
+                {doc.aff && doc.aff[i] && (
+                  <meta name="citation_author_institution" content={doc.aff[i]} data-highwire="true" />
+                )}
+              </Fragment>
+            ))}
 
-      {doc.title && <meta name="citation_title" content={title} data-highwire="true" />}
+          {doc.title && <meta name="citation_title" content={title} data-highwire="true" />}
 
-      {doc.volume && <meta name="citation_volume" content={doc.volume} data-highwire="true" />}
+          {doc.volume && <meta name="citation_volume" content={doc.volume} data-highwire="true" />}
 
-      {doc.issue && <meta name="citation_issue" content={doc.issue} data-highwire="true" />}
+          {doc.issue && <meta name="citation_issue" content={doc.issue} data-highwire="true" />}
 
-      {doc.page && <meta name="citation_firstpage" content={doc.page} data-highwire="true" />}
+          {doc.page && <meta name="citation_firstpage" content={doc.page} data-highwire="true" />}
 
-      {doc.doi && doc.doi.length > 0 && <meta name="citation_doi" content={doc.doi[0]} data-highwire="true" />}
+          {doc.doi && doc.doi.length > 0 && <meta name="citation_doi" content={doc.doi[0]} data-highwire="true" />}
 
-      {doc.issn && doc.issn.length > 0 && <meta name="citation_issn" content={doc.issn[0]} data-highwire="true" />}
+          {doc.issn && doc.issn.length > 0 && <meta name="citation_issn" content={doc.issn[0]} data-highwire="true" />}
 
-      {doc.isbn && doc.isbn.length > 0 && <meta name="citation_isbn" content={doc.isbn[0]} data-highwire="true" />}
+          {doc.isbn && doc.isbn.length > 0 && <meta name="citation_isbn" content={doc.isbn[0]} data-highwire="true" />}
 
-      <meta name="citation_language" content="en" data-highwire="true" />
+          <meta name="citation_language" content="en" data-highwire="true" />
 
-      {doc.keyword &&
-        doc.keyword.map((kw, i) => (
-          <meta key={`keyword-${i}`} name="citation_keywords" content={kw} data-highwire="true" />
-        ))}
+          {doc.keyword &&
+            doc.keyword.map((kw, i) => (
+              <meta key={`keyword-${i}`} name="citation_keywords" content={kw} data-highwire="true" />
+            ))}
 
-      {doc.doctype === 'PhD Thesis' && <meta name="citation_dissertation_name" content="Phd" data-highwire="true" />}
+          {normalizedDoctype === 'phdthesis' && (
+            <meta name="citation_dissertation_name" content="Phd" data-highwire="true" />
+          )}
 
-      {doc.doctype === 'Masters Thesis' && <meta name="citation_dissertation_name" content="MS" data-highwire="true" />}
+          {normalizedDoctype === 'mastersthesis' && (
+            <meta name="citation_dissertation_name" content="MS" data-highwire="true" />
+          )}
 
-      <meta name="citation_abstract_html_url" content={url} data-highwire="true" />
+          <meta name="citation_abstract_html_url" content={url} data-highwire="true" />
 
-      {doc.pubdate && <meta name="citation_publication_date" content={citation_date} data-highwire="true" />}
+          {doc.pubdate && <meta name="citation_publication_date" content={citation_date} data-highwire="true" />}
 
-      {doc.esources && doc.esources.find((e) => e === Esources.PUB_PDF) && (
-        <meta name="citation_pdf_url" content={`${LINKGATEWAY_BASE_URL}/${doc.bibcode}/PUB_PDF`} data-highwire="true" />
+          {doc.esources && doc.esources.find((e) => e === Esources.PUB_PDF) && (
+            <meta
+              name="citation_pdf_url"
+              content={`${LINKGATEWAY_BASE_URL}/${doc.bibcode}/PUB_PDF`}
+              data-highwire="true"
+            />
+          )}
+
+          {doc.page_range && <meta name="citation_lastpage" content={last_page} data-highwire="true" />}
+
+          {arXiv !== '' && <meta name="citation_arxiv_id" content={arXiv} data-highwire="true" />}
+
+          <link title="schema(PRISM)" rel="schema.prism" href="http://prismstandard.org/namespaces/1.2/basic/" />
+
+          {doc.pubdate && <meta name="prism.publicationDate" content={formatted_numeric_pubdate} />}
+
+          {doc.bibstem && doc.bibstem.length > 0 && <meta name="prism.publicationName" content={doc.bibstem[0]} />}
+
+          {doc.issn && doc.issn.length > 0 && <meta name="prism.issn" content={doc.issn[0]} />}
+
+          {doc.volume && <meta name="prism.volume" content={doc.volume} />}
+
+          {doc.page && <meta name="prism.startingPage" content={doc.page} />}
+
+          {doc.page_range && <meta name="prism.endingPage" content={last_page} />}
+
+          <link title="schema(DC)" rel="schema.dc" href="http://purl.org/dc/elements/1.1/" />
+
+          {doc.doi && doc.doi.length > 0 && <meta name="dc.identifier" content={`doi:${doc.doi[0]}`} />}
+
+          {doc.pubdate && <meta name="dc.date" content={formatted_numeric_pubdate} />}
+
+          {doc.bibstem && doc.bibstem.length > 0 && <meta name="dc.source" content={doc.bibstem[0]} />}
+
+          <meta name="dc.title" content={title} />
+
+          {doc.author && authors.map((a, i) => <meta key={`dcc-${i}`} name="dc.creator" content={a} />)}
+        </>
       )}
-
-      {doc.page_range && <meta name="citation_lastpage" content={last_page} data-highwire="true" />}
-
-      {arXiv !== '' && <meta name="citation_arxiv_id" content={arXiv} data-highwire="true" />}
-
-      <link title="schema(PRISM)" rel="schema.prism" href="http://prismstandard.org/namespaces/1.2/basic/" />
-
-      {doc.pubdate && <meta name="prism.publicationDate" content={formatted_numeric_pubdate} />}
-
-      {doc.bibstem && doc.bibstem.length > 0 && <meta name="prism.publicationName" content={doc.bibstem[0]} />}
-
-      {doc.issn && doc.issn.length > 0 && <meta name="prism.issn" content={doc.issn[0]} />}
-
-      {doc.volume && <meta name="prism.volume" content={doc.volume} />}
-
-      {doc.page && <meta name="prism.startingPage" content={doc.page} />}
-
-      {doc.page_range && <meta name="prism.endingPage" content={last_page} />}
-
-      <link title="schema(DC)" rel="schema.dc" href="http://purl.org/dc/elements/1.1/" />
-
-      {doc.doi && doc.doi.length > 0 && <meta name="dc.identifier" content={`doi:${doc.doi[0]}`} />}
-
-      {doc.pubdate && <meta name="dc.date" content={formatted_numeric_pubdate} />}
-
-      {doc.bibstem && doc.bibstem.length > 0 && <meta name="dc.source" content={doc.bibstem[0]} />}
-
-      <meta name="dc.title" content={title} />
-
-      {doc.author && authors.map((a, i) => <meta key={`dcc-${i}`} name="dc.creator" content={a} />)}
 
       <meta name="twitter:card" content="summary" data-highwire="true" />
 
